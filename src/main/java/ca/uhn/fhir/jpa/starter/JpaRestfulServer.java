09141dd64b8f62b5433dc8d9bf1d5126b7d2d535
Sean McIlvenna <sean.mcilvenna@lantanagroup.com>
2019-05-05 10:49:24 -0400

    Adding the "Prefer" header as an allowed header by cors when cors is enabled

--- src/main/java/ca/uhn/fhir/jpa/starter/JpaRestfulServer.java
+++ src/main/java/ca/uhn/fhir/jpa/starter/JpaRestfulServer.java
@@ -179,6 +179,7 @@ public class JpaRestfulServer extends RestfulServer {
             config.addAllowedHeader("Accept");
             config.addAllowedHeader("X-Requested-With");
             config.addAllowedHeader("Content-Type");
+            config.addAllowedHeader("Prefer");
 
             config.addAllowedOrigin(HapiProperties.getCorsAllowedOrigin());
 
