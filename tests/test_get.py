6d885335cd30ce8012f3f71e1e22fca9e26d6dd7
Durant Schoon <durant.schoon@freenome.com>
2020-02-19 10:25:44 -0800

    fix spelling

--- tests/test_get.py
+++ tests/test_get.py
@@ -5,7 +5,7 @@ import requests
 URL = "http://localhost:8080/hapi-fhir-jpaserver/fhir/"
 
 
-def test_GET_organizaiton_random_unknown_name_fail():
+def test_GET_organization_random_unknown_name_fail():
     non_existent_name = f"SHOULD-NOT-EXIST-{random.random()}"
     # URL similar to "http://localhost:8080/hapi-fhir-jpaserver/fhir/Organization?name=SHOULD-NOT-EXIST-0.9383671425079436"
     response = requests.get(URL + "Organization", params={"name": non_existent_name})
