5d0b26bbf33a4bd7ddfc2e2aaefd8b3896e42722
Tadgh <garygrantgraham@gmail.com>
2020-07-09 10:00:00 -0700

    Have EmpiSettings actually return an EmpiSettings bean

--- src/main/java/ca/uhn/fhir/jpa/starter/EmpiConfig.java
+++ src/main/java/ca/uhn/fhir/jpa/starter/EmpiConfig.java
@@ -1,9 +1,15 @@
 package ca.uhn.fhir.jpa.starter;
 
+import ca.uhn.fhir.context.FhirContext;
 import ca.uhn.fhir.empi.api.IEmpiSettings;
+import ca.uhn.fhir.empi.rules.config.EmpiRuleValidator;
 import ca.uhn.fhir.empi.rules.config.EmpiSettings;
+import ca.uhn.fhir.jpa.empi.svc.EmpiSearchParamSvc;
+import ca.uhn.fhir.jpa.subscription.channel.subscription.IChannelNamer;
+import ca.uhn.fhir.rest.server.util.ISearchParamRetriever;
 import com.google.common.base.Charsets;
 import org.apache.commons.io.IOUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.DefaultResourceLoader;
@@ -15,11 +21,11 @@ import java.io.IOException;
 public class EmpiConfig {
 
   @Bean
-  IEmpiSettings empiSettings() throws IOException {
+  public IEmpiSettings empiSettings(EmpiRuleValidator theEmpiRuleValidator) throws IOException {
     DefaultResourceLoader resourceLoader = new DefaultResourceLoader();
     Resource resource = resourceLoader.getResource("empi-rules.json");
     String json = IOUtils.toString(resource.getInputStream(), Charsets.UTF_8);
-    return null; // new EmpiSettings().setEnabled(HapiProperties.getEmpiEnabled()).setScriptText(json);
+    return  new EmpiSettings(theEmpiRuleValidator).setEnabled(HapiProperties.getEmpiEnabled()).setScriptText(json);
   }
 
 }
