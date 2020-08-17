49c8b71adf661b5ce2dcddae31389ae80fdc3629
jvi <jvi@trifork.com>
2020-08-17 20:36:20 +0200

    remove empi as the current configuration throws exceptions:
    [main] ERROR o.s.web.context.ContextLoader [ContextLoader.java:313] Context initialization failed
    org.springframework.beans.factory.UnsatisfiedDependencyException: Error creating bean with name 'empiSettings' defined in class path resource [ca/uhn/fhir/jpa/starter/EmpiConfig.class]: Unsatisfied dependency expressed through method 'empiSettings' parameter 0; nested exception is org.springframework.beans.factory.NoSuchBeanDefinitionException: No qualifying bean of type 'ca.uhn.fhir.empi.rules.config.EmpiRuleValidator' available: expected at least 1 bean which qualifies as autowire candidate. Dependency annotations: {}
    	at org.springframework.beans.factory.support.ConstructorResolver.createArgumentArray(ConstructorResolver.java:798)
    	at org.springframework.beans.factory.support.ConstructorResolver.instantiateUsingFactoryMethod(ConstructorResolver.java:539)

--- src/main/java/ca/uhn/fhir/jpa/starter/EmpiConfig.java
+++ src/main/java/ca/uhn/fhir/jpa/starter/EmpiConfig.java
@@ -1,22 +1,17 @@
 package ca.uhn.fhir.jpa.starter;
 
-import ca.uhn.fhir.context.FhirContext;
 import ca.uhn.fhir.empi.api.IEmpiSettings;
 import ca.uhn.fhir.empi.rules.config.EmpiRuleValidator;
 import ca.uhn.fhir.empi.rules.config.EmpiSettings;
-import ca.uhn.fhir.jpa.empi.svc.EmpiSearchParamSvc;
-import ca.uhn.fhir.jpa.subscription.channel.subscription.IChannelNamer;
-import ca.uhn.fhir.rest.server.util.ISearchParamRetriever;
 import com.google.common.base.Charsets;
 import org.apache.commons.io.IOUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.DefaultResourceLoader;
 import org.springframework.core.io.Resource;
 
 import java.io.IOException;
-
+/*
 @Configuration
 public class EmpiConfig {
 
@@ -29,3 +24,4 @@ public class EmpiConfig {
   }
 
 }
+*/
