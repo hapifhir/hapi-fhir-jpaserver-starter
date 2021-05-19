877e3883abf9619a80464d33a5fb1ac76b099fda
jamesagnew <jamesagnew@gmail.com>
2021-05-19 08:45:03 -0400

    Compile fixes

--- src/main/java/ca/uhn/fhir/jpa/mdm/MdmConfig.java
+++ src/main/java/ca/uhn/fhir/jpa/mdm/MdmConfig.java
@@ -7,7 +7,7 @@ import ca.uhn.fhir.jpa.starter.AppProperties;
 import ca.uhn.fhir.mdm.api.IMdmSettings;
 import ca.uhn.fhir.mdm.rules.config.MdmRuleValidator;
 import ca.uhn.fhir.mdm.rules.config.MdmSettings;
-import ca.uhn.fhir.rest.server.util.ISearchParamRetriever;
+import ca.uhn.fhir.rest.server.util.ISearchParamRegistry;
 import com.google.common.base.Charsets;
 import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,17 +29,17 @@ import java.io.IOException;
 @Import({MdmConsumerConfig.class, MdmSubmitterConfig.class})
 public class MdmConfig {
 
-  @Bean
-  MdmRuleValidator mdmRuleValidator(FhirContext theFhirContext, ISearchParamRetriever theSearchParamRetriever) {
-    return new MdmRuleValidator(theFhirContext, theSearchParamRetriever);
-  }
+	@Bean
+	MdmRuleValidator mdmRuleValidator(FhirContext theFhirContext, ISearchParamRegistry theSearchParamRegistry) {
+		return new MdmRuleValidator(theFhirContext, theSearchParamRegistry);
+	}
 
-  @Bean
-  IMdmSettings mdmSettings(@Autowired MdmRuleValidator theMdmRuleValidator, AppProperties appProperties) throws IOException {
-    DefaultResourceLoader resourceLoader = new DefaultResourceLoader();
-    Resource resource = resourceLoader.getResource("mdm-rules.json");
-    String json = IOUtils.toString(resource.getInputStream(), Charsets.UTF_8);
-    return new MdmSettings(theMdmRuleValidator).setEnabled(appProperties.getMdm_enabled()).setScriptText(json);
-  }
+	@Bean
+	IMdmSettings mdmSettings(@Autowired MdmRuleValidator theMdmRuleValidator, AppProperties appProperties) throws IOException {
+		DefaultResourceLoader resourceLoader = new DefaultResourceLoader();
+		Resource resource = resourceLoader.getResource("mdm-rules.json");
+		String json = IOUtils.toString(resource.getInputStream(), Charsets.UTF_8);
+		return new MdmSettings(theMdmRuleValidator).setEnabled(appProperties.getMdm_enabled()).setScriptText(json);
+	}
 
 }
