5ab56d32ebd5a92d1f26f858308e364b47c9dc57
Reham Muzzamil <reham.muzzamil@venturedive.com>
2021-08-27 00:39:34 +0500

    Add hapi-fhir-opensrp-extensions dependency for Location Hierarchy fetch API

--- src/main/java/ca/uhn/fhir/jpa/starter/JpaRestfulServer.java
+++ src/main/java/ca/uhn/fhir/jpa/starter/JpaRestfulServer.java
@@ -1,5 +1,9 @@
 package ca.uhn.fhir.jpa.starter;
 
+import ca.uhn.fhir.jpa.api.dao.IFhirResourceDao;
+import org.hl7.fhir.r4.model.Location;
+import org.smartregister.extension.model.*;
+import org.smartregister.extension.rest.LocationHierarchyResourceProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Import;
 
@@ -20,6 +24,18 @@ public class JpaRestfulServer extends BaseJpaRestfulServer {
   @Override
   protected void initialize() throws ServletException {
     super.initialize();
+	  IFhirResourceDao<Location> locationIFhirResourceDao = daoRegistry.getResourceDao("Location");
+	  LocationHierarchyResourceProvider locationHierarchyResourceProvider = new LocationHierarchyResourceProvider();
+	  locationHierarchyResourceProvider.setLocationIFhirResourceDao(locationIFhirResourceDao);
+
+	  registerProvider(locationHierarchyResourceProvider);
+	  getFhirContext().registerCustomType(LocationHierarchy.class);
+	  getFhirContext().registerCustomType(LocationHierarchyTree.class);
+	  getFhirContext().registerCustomType(Tree.class);
+	  getFhirContext().registerCustomType(ParentChildrenMap.class);
+	  getFhirContext().registerCustomType(SingleTreeNode.class);
+	  getFhirContext().registerCustomType(TreeNode.class);
+	  getFhirContext().registerCustomType(ChildTreeNode.class);
 
     // Add your own customization here
 
