f1fe256a0964137f11dfc1b4ae44bf12dded2de7
Oliver Egger <oliver.egger@ahdis.ch>
2022-01-04 15:56:08 +0100

    extend npm ig handling #23

--- src/main/java/ch/ahdis/fhir/hapi/jpa/validation/ImplementationGuideProvider.java
+++ src/main/java/ch/ahdis/fhir/hapi/jpa/validation/ImplementationGuideProvider.java
@@ -5,8 +5,11 @@ import java.util.Optional;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.hl7.fhir.instance.model.api.IIdType;
 import org.hl7.fhir.r4.model.ImplementationGuide;
 import org.hl7.fhir.r4.model.OperationOutcome;
+import org.hl7.fhir.r4.model.OperationOutcome.IssueSeverity;
+import org.hl7.fhir.r4.model.OperationOutcome.IssueType;
 import org.quartz.DisallowConcurrentExecution;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
@@ -21,6 +24,7 @@ import org.springframework.transaction.support.TransactionTemplate;
 
 import ca.uhn.fhir.jpa.dao.data.INpmPackageVersionDao;
 import ca.uhn.fhir.jpa.model.entity.NpmPackageVersionEntity;
+import ca.uhn.fhir.jpa.packages.PackageDeleteOutcomeJson;
 import ca.uhn.fhir.jpa.packages.PackageInstallOutcomeJson;
 import ca.uhn.fhir.jpa.packages.PackageInstallationSpec;
 import ca.uhn.fhir.jpa.starter.AppProperties;
@@ -37,13 +41,33 @@ import ch.ahdis.matchbox.util.MatchboxPackageInstallerImpl;
 public class ImplementationGuideProvider extends ca.uhn.fhir.jpa.rp.r4.ImplementationGuideResourceProvider implements  Job, ApplicationContextAware {
 
   @Override
-  public MethodOutcome create(HttpServletRequest theRequest, ImplementationGuide theResource, String theConditional,
+  public MethodOutcome delete(HttpServletRequest theRequest, IIdType theResource, String theConditional,
       RequestDetails theRequestDetails) {
-    OperationOutcome outcome = load(theResource);
-    if (outcome==null) {
-      return super.create(theRequest, theResource, theConditional, theRequestDetails);
+    ImplementationGuide guide = this.getDao().read(theResource);
+    OperationOutcome oo = uninstall(guide);
+    MethodOutcome outcome =   super.delete(theRequest, theResource, theConditional, theRequestDetails);
+    if (oo!=null) {
+      outcome.setOperationOutcome(oo);
     }
-    return new MethodOutcome(outcome);
+    return outcome;
+  }
+
+  @Override
+  public MethodOutcome update(HttpServletRequest theRequest, ImplementationGuide theResource, IIdType theId,
+      String theConditional, RequestDetails theRequestDetails) {
+    OperationOutcome oo = load(theResource);
+    MethodOutcome outcome =  super.update(theRequest, theResource, theId, theConditional, theRequestDetails);
+    outcome.setOperationOutcome(oo);
+    return outcome;
+  }
+
+  @Override
+  public MethodOutcome create(HttpServletRequest theRequest, ImplementationGuide theResource, String theConditional,
+    RequestDetails theRequestDetails) {
+    OperationOutcome oo = load(theResource);
+    MethodOutcome outcome =  super.create(theRequest, theResource, theConditional, theRequestDetails);
+    outcome.setOperationOutcome(oo);
+    return outcome;
   }
 
   @Autowired
@@ -57,9 +81,41 @@ public class ImplementationGuideProvider extends ca.uhn.fhir.jpa.rp.r4.Implement
 
   @Autowired
   private PlatformTransactionManager myTxManager;
+  
+  public OperationOutcome getOperationOutcome(PackageInstallOutcomeJson pkgOutcome) {
+    if (pkgOutcome==null) {
+      return null;
+    }
+    OperationOutcome outcome = new OperationOutcome();
+    for(String message: pkgOutcome.getMessage())  {
+      outcome.addIssue().setSeverity(IssueSeverity.INFORMATION).setCode(IssueType.PROCESSING).setDiagnostics(message);
+    }
+    for(String resource: pkgOutcome.getResourcesInstalled().keySet())  {
+      outcome.addIssue().setSeverity(IssueSeverity.INFORMATION).setCode(IssueType.PROCESSING).setDiagnostics(resource + ": "+pkgOutcome.getResourcesInstalled().get(resource));
+    }
+    return outcome;
+  }
 
-	public OperationOutcome load(ImplementationGuide theResource) {
-    packageInstallerSvc.install(new PackageInstallationSpec()
+  public OperationOutcome getOperationOutcome(PackageDeleteOutcomeJson pkgOutcome) {
+    if (pkgOutcome==null) {
+      return null;
+    }
+    OperationOutcome outcome = new OperationOutcome();
+    for(String message: pkgOutcome.getMessage())  {
+      outcome.addIssue().setSeverity(IssueSeverity.INFORMATION).setCode(IssueType.PROCESSING).setDiagnostics(message);
+    }
+    return outcome;
+  }
+
+  public OperationOutcome uninstall(ImplementationGuide theResource) {
+    return getOperationOutcome(packageInstallerSvc.uninstall(new PackageInstallationSpec()
+        .setPackageUrl(theResource.getUrl())
+        .setName(theResource.getName())
+        .setVersion(theResource.getVersion())));
+  }
+
+  public PackageInstallOutcomeJson load(ImplementationGuide theResource, PackageInstallOutcomeJson install) {
+    PackageInstallOutcomeJson installOutcome = packageInstallerSvc.install(new PackageInstallationSpec()
         .setPackageUrl(theResource.getUrl())
         .addInstallResourceTypes("NamingSystem",
             "CodeSystem",
@@ -73,10 +129,33 @@ public class ImplementationGuideProvider extends ca.uhn.fhir.jpa.rp.r4.Implement
         .setName(theResource.getName())
         .setVersion(theResource.getVersion())
           .setInstallMode(PackageInstallationSpec.InstallModeEnum.STORE_AND_INSTALL));
-    return null;
+    if (install != null) {
+      install.getMessage().addAll(installOutcome.getMessage());
+      return install;
+    }
+    return installOutcome;
+  }
+
+	public OperationOutcome load(ImplementationGuide theResource) {
+	  PackageInstallOutcomeJson installOutcome = packageInstallerSvc.install(new PackageInstallationSpec()
+        .setPackageUrl(theResource.getUrl())
+        .addInstallResourceTypes("NamingSystem",
+            "CodeSystem",
+            "ValueSet",
+            "StructureDefinition",
+            "ConceptMap",
+            "SearchParameter",
+            "Subscription",
+            "StructureMap",
+            "Questionnaire")
+        .setName(theResource.getName())
+        .setVersion(theResource.getVersion())
+          .setInstallMode(PackageInstallationSpec.InstallModeEnum.STORE_AND_INSTALL));
+    return getOperationOutcome(installOutcome);
 	}
 	
-	public void loadAll(boolean replace) {
+	public PackageInstallOutcomeJson loadAll(boolean replace) {
+	  PackageInstallOutcomeJson installOutcome = null;
     if (appProperties.getImplementationGuides() != null) {
       Map<String, AppProperties.ImplementationGuide> guides = appProperties.getImplementationGuides();
       for (AppProperties.ImplementationGuide guide : guides.values()) {
@@ -90,20 +169,17 @@ public class ImplementationGuideProvider extends ca.uhn.fhir.jpa.rp.r4.Implement
           ig.setPackageId(guide.getName());
           ig.setUrl(guide.getUrl());
           ig.setVersion(guide.getVersion());
-          OperationOutcome outcome = load(ig);
-          if (outcome==null) {
-            packageInstallerSvc.create(ig,new PackageInstallOutcomeJson());
-          }
+          installOutcome = load(ig, installOutcome);
+          packageInstallerSvc.create(ig, installOutcome);
         }
       }
     }
+    return installOutcome;
 	}
 	
 	@Operation(name = "$load-all", type = ImplementationGuide.class, idempotent = false)
 	public OperationOutcome loadAll() {
-	  this.loadAll(true);
-		  OperationOutcome outcome = new OperationOutcome();
-	      return outcome;
+	  return this.getOperationOutcome(loadAll(true));
 	}
 
   @Override
