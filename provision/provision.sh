a88f0dec684a13bd920c8abf87653ffe1464ea35
connor <connorsmyth@rogers.com>
2022-01-04 14:47:17 -0500

    Fixing provision script

--- provision/provision.sh
+++ provision/provision.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+CURRENT_DIR=$(pwd)
+JSON=/provision/combined-bundles.json
+FILE=$CURRENT_DIR$JSON
+
 curl -X POST \
   -H "Content-Type: application/json" \
-  -d @./combined-bundles.json \
+  -d @$FILE \
   http://localhost:8080/fhir
