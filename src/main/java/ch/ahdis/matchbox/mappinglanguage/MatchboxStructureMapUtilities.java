9209f9106e6b05375d24cd5cee4d03e154568990
Oliver Egger <oliver.egger@ahdis.ch>
2022-01-13 11:37:04 +0100

    fix typo licanse

--- src/main/java/ch/ahdis/matchbox/mappinglanguage/MatchboxStructureMapUtilities.java
+++ src/main/java/ch/ahdis/matchbox/mappinglanguage/MatchboxStructureMapUtilities.java
@@ -1,5 +1,21 @@
-/**
+ /*
+ * #%L
+ * Matchbox
+ * %%
  * Copyright (c) 2022- by RALY GmbH. All rights reserved.
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
  */
 package ch.ahdis.matchbox.mappinglanguage;
 
@@ -30,7 +46,7 @@ import org.hl7.fhir.r5.utils.structuremap.TransformContext;
 import org.jboss.logging.Logger;
 
 /**
- * Class to overwrite tranlation method to fix certain problems with CDA2FHIR
+ * Class to overwrite translation method to fix certain problems with CDA2FHIR
  * mapping
  */
 public class MatchboxStructureMapUtilities extends StructureMapUtilities {
