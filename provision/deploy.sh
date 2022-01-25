42f91d6d240845e9319acbe7df8fbdf07e22563d
connor <connorsmyth@rogers.com>
2022-01-25 13:30:03 -0500

    Updating deploy script

--- provision/deploy.sh
+++ provision/deploy.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
-# Stop and remove container
-docker stop hapi_fhir_server
-# shellcheck disable=SC2046
-docker container rm $(docker container ls -aq) -f
-
-# Login to aws
+# login to aws
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 105227342372.dkr.ecr.us-east-1.amazonaws.com
 
-# Pull latest image
+# pull latest image
 docker pull 105227342372.dkr.ecr.us-east-1.amazonaws.com/junipercds-hapi-fhir:latest
 
-# Create container
+# stop container
+docker stop hapi_fhir_server
+
+# remove container
+docker container rm $(docker container ls -aq) -f
+
+# run new container
 docker run -p 8080:8080 -d --name hapi_fhir_server 105227342372.dkr.ecr.us-east-1.amazonaws.com/junipercds-hapi-fhir:latest
 
-# Remove dangling images (cleanup)
-# shellcheck disable=SC2046
-docker image rm $(docker image ls -f "dangling=true" -q) -f
+# remove dangling images
+docker image rm $(docker image ls -f dangling=true -q) -f
+
+# reload nginx
+nginx -t && service nginx reload
 
