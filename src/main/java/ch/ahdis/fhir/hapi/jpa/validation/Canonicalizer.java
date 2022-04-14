913ebeecd4c44387fe8ed5bd0333166c79597f5c
Oliver Egger <oliver.egger@ahdis.ch>
2022-04-14 13:11:35 +0200

    comparing examples

--- src/main/java/ch/ahdis/fhir/hapi/jpa/validation/Canonicalizer.java
+++ src/main/java/ch/ahdis/fhir/hapi/jpa/validation/Canonicalizer.java
@@ -1,6 +1,11 @@
 package ch.ahdis.fhir.hapi.jpa.validation;
 
+import java.util.Collection;
 import java.util.Comparator;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
 
 import org.apache.commons.lang3.Validate;
 import org.hl7.fhir.instance.model.api.IBase;
@@ -12,7 +17,13 @@ import ca.uhn.fhir.context.BaseRuntimeChildDefinition;
 import ca.uhn.fhir.context.FhirContext;
 import ca.uhn.fhir.context.RuntimeResourceDefinition;
 import ca.uhn.fhir.i18n.Msg;
+import ca.uhn.fhir.interceptor.model.RequestPartitionId;
+import ca.uhn.fhir.jpa.model.entity.ResourceTable;
+import ca.uhn.fhir.jpa.searchparam.extractor.ResourceIndexedSearchParams;
+import ca.uhn.fhir.rest.api.server.RequestDetails;
+import ca.uhn.fhir.rest.api.server.storage.TransactionDetails;
 import ca.uhn.fhir.util.FhirTerser;
+import ca.uhn.fhir.util.ResourceReferenceInfo;
 
 public class Canonicalizer {
 
@@ -29,6 +40,13 @@ public class Canonicalizer {
   private static Comparator<IBaseExtension<?, ?>> extensionComparator = Comparator
       .comparing(IBaseExtension<?, ?>::getUrl, nullSafeStringComparator);
   
+  public void sortExtensionRecursive(IBase ibase) {
+    IBaseHasExtensions baseHasExtensions = validateExtensionSupport(ibase);
+    baseHasExtensions.getExtension().sort(extensionComparator);
+    for(IBaseExtension<?, ?> extension : baseHasExtensions.getExtension()) {
+      sortExtensionRecursive(extension);
+    }
+  }
   
 
   public IBaseResource canonicalize(IBaseResource input) {
@@ -39,19 +57,60 @@ public class Canonicalizer {
 
     // DomainResource TODO
 
-    // ... text 0..1 Narrative Text summary of the resource, for human
+    // TODO ... text 0..1 Narrative Text summary of the resource, for human
     // interpretation
-    // ... contained 0..* Resource Contained, inline Resources
+    // TODO ... contained 0..* Resource Contained, inline Resources
+//    this.fhirTerser.getAllResourceReferences(theResource)
+
+//    or maybe 
+//    
+//    private void extractResourceLinksForContainedResources(RequestPartitionId theRequestPartitionId, ResourceIndexedSearchParams theParams, ResourceTable theEntity, IBaseResource theResource, TransactionDetails theTransactionDetails, boolean theFailOnInvalidReference, RequestDetails theRequest) {
+//
+//      FhirTerser terser = myContext.newTerser();
+//
+//      // 1. get all contained resources
+//      Collection<IBaseResource> containedResources = terser.getAllEmbeddedResources(theResource, false);
+//
+//      extractResourceLinksForContainedResources(theRequestPartitionId, theParams, theEntity, theResource, theTransactionDetails, theFailOnInvalidReference, theRequest, containedResources, new HashSet<>());
+//    }
+//
+//    private void extractResourceLinksForContainedResources(RequestPartitionId theRequestPartitionId, ResourceIndexedSearchParams theParams, ResourceTable theEntity, IBaseResource theResource, TransactionDetails theTransactionDetails, boolean theFailOnInvalidReference, RequestDetails theRequest, Collection<IBaseResource> theContainedResources, Collection<IBaseResource> theAlreadySeenResources) {
+
+      // 2
+
+    // sort contained resource depth first
+    // check all references in the resource which start with an # (contained resource), for each reference that is not yet in the contained list
+    List<ResourceReferenceInfo> resourceReferences = this.fhirTerser.getAllResourceReferences(input);
+    Collection<IBaseResource> containedResources = this.fhirTerser.getAllEmbeddedResources(input, false);
+    
+    final Map<String, String> ids = new  HashMap<String, String>();
+    int contained = 0;
+    for (ResourceReferenceInfo resourceReference : resourceReferences) {
+      String refContainted = resourceReference.getResourceReference().getReferenceElement().getValue();
+      if (!resourceReference.getName().startsWith("contained") && refContainted.startsWith("#") && refContainted.length()>1) {
+        String containedId = refContainted.substring(1);
+        if (ids.get(containedId)==null) {
+          String newContainedId = "c"+contained++;
+          ids.put(containedId, newContainedId);
+          //   add the contained resource, (replace id?)
+          //     for the contained resource check all references in the resource which start with an #, add the contained resource ...
+          // check contained resource itself
+        }
+      }
+      
+    }
+    //   add the contained resource, (replace id?)
+    //     for the contained resource check all references in the resource which start with an #, add the contained resource ...
+    // check contained resource itself
+    
     // ... extension 0..* Extension Additional content defined by implementations
-    // modifierExtension ?! 0..* Extension Extensions that cannot be ignored
-    IBaseHasExtensions baseHasExtensions = validateExtensionSupport(input);
-    baseHasExtensions.getExtension().sort(extensionComparator);
+    // sort in complex extensions according to the url
+    sortExtensionRecursive(input);
     
-    // TODO: sort in complex extensions according to the url
+    // TODO modifierExtension ?! 0..* Extension Extensions that cannot be ignored
     
-    // TODO: if we have * cardinality of a specific extension, we would need to
-    // order them also, do we need do that really? or should we look that the
-    // orginal sort order is retained?
+    // TODO: if we have * cardinality of a specific extension should we order?
+    
 
     return input;
   }
--- src/test/java/ch/ahdis/validation/CanonicalizerTests.java
+++ src/test/java/ch/ahdis/validation/CanonicalizerTests.java
@@ -35,7 +35,7 @@ public class CanonicalizerTests {
     try {
       contentString = this.getContent("canonicalize-elements.json");
     } catch(Exception e) {
-      assertTrue("catched excepiton", false);
+      assertTrue("catched exception", false);
     }
     
     FhirContext contextR4 = FhirVersionEnum.R4.newContext();
@@ -65,7 +65,7 @@ public class CanonicalizerTests {
     try {
       contentString = this.getContent("canonicalize-patient.json");
     } catch(Exception e) {
-      assertTrue("catched excepiton", false);
+      assertTrue("catched exception", false);
     }
     
     FhirContext contextR4 = FhirVersionEnum.R4.newContext();
@@ -78,8 +78,30 @@ public class CanonicalizerTests {
 
     assertEquals("http://hl7.org/fhir/StructureDefinition/patient-birthPlace", fhirPath.evaluateFirst(result, "extension[0].url", IPrimitiveType.class).get().getValueAsString());
     assertEquals("http://hl7.org/fhir/StructureDefinition/patient-citizenship", fhirPath.evaluateFirst(result, "extension[1].url", IPrimitiveType.class).get().getValueAsString());
+    assertEquals("code", fhirPath.evaluateFirst(result, "extension[1].extension[0].url", IPrimitiveType.class).get().getValueAsString());
+    assertEquals("period", fhirPath.evaluateFirst(result, "extension[1].extension[1].url", IPrimitiveType.class).get().getValueAsString());
     
   }
+  
+  @Test
+  public void chargeItem() {
+    
+    String contentString = "";
+    try {
+      contentString = this.getContent("canonicalize-chargeitem.json");
+    } catch(Exception e) {
+      assertTrue("catched exception", false);
+    }
+    
+    FhirContext contextR4 = FhirVersionEnum.R4.newContext();
+    IBaseResource resource = contextR4.newJsonParser().parseResource(contentString);
+    
+    Canonicalizer canonicalizer= new Canonicalizer(contextR4);
+    IBaseResource result =  canonicalizer.canonicalize(resource);
+    IFhirPath fhirPath = contextR4.newFhirPath();
+
+  }
+
 
   
   private static BaseRuntimeChildDefinition getBaseRuntimeChildDefinition(FhirContext theFhirContext, String theFieldName, IBaseResource theFrom) {
