5096fa991752ddf9634782658c2863bb4a6ead1f
Kashyap Jois <kjois@iprdgroup.com>
2022-06-20 14:54:55 +0530

    Fix bugs and change group id

--- src/main/java/ca/uhn/fhir/jpa/starter/service/HelperService.java
+++ src/main/java/ca/uhn/fhir/jpa/starter/service/HelperService.java
@@ -40,7 +40,6 @@ import org.springframework.context.annotation.Import;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
