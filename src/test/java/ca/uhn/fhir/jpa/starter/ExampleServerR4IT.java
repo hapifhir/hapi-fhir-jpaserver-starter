43d50a0c71bb5c3194c05a17db74386773c518f2
Jens Kristian Villadsen <jenskristianvilladsen@gmail.com>
2022-09-13 18:54:14 +0200

    Extract IG loading (#426)

--- src/test/java/ca/uhn/fhir/jpa/starter/ExampleServerR4IT.java
+++ src/test/java/ca/uhn/fhir/jpa/starter/ExampleServerR4IT.java
@@ -37,6 +37,8 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 		"hapi.fhir.fhir_version=r4",
 		"hapi.fhir.subscription.websocket_enabled=true",
 		"hapi.fhir.mdm_enabled=true",
+		"hapi.fhir.implementationguides.dk-core.name=hl7.fhir.dk.core",
+		"hapi.fhir.implementationguides.dk-core.version=1.1.0",
 		// Override is currently required when using MDM as the construction of the MDM
 		// beans are ambiguous as they are constructed multiple places. This is evident
 		// when running in a spring boot environment
