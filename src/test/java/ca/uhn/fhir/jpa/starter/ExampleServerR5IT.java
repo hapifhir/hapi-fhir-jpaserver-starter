b8f4c147056cdcdab6d80e3c732870987b00683f
Tadgh <garygrantgraham@gmail.com>
2022-10-28 12:09:08 -0700

    remove batch refs

--- src/test/java/ca/uhn/fhir/jpa/starter/ExampleServerR5IT.java
+++ src/test/java/ca/uhn/fhir/jpa/starter/ExampleServerR5IT.java
@@ -34,7 +34,6 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = Application.class, properties =
   {
-     "spring.batch.job.enabled=false",
      "spring.datasource.url=jdbc:h2:mem:dbr5",
      "hapi.fhir.fhir_version=r5",
      "hapi.fhir.subscription.websocket_enabled=true",
