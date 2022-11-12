2e1f5f52769cf5f38883f3ac27e20f1fe1bdd84a
Jens Kristian Villadsen <jenskristianvilladsen@gmail.com>
2022-11-12 18:39:38 +0100

    fixes for support of R4B / 6.2.0 (#455)

---
+++ src/main/java/ca/uhn/fhir/jpa/starter/annotations/OnR4BCondition.java
@@ -0,0 +1,18 @@
+package ca.uhn.fhir.jpa.starter.annotations;
+
+import ca.uhn.fhir.context.FhirVersionEnum;
+import org.springframework.context.annotation.Condition;
+import org.springframework.context.annotation.ConditionContext;
+import org.springframework.core.type.AnnotatedTypeMetadata;
+
+public class OnR4BCondition implements Condition {
+	@Override
+	public boolean matches(ConditionContext conditionContext, AnnotatedTypeMetadata metadata) {
+		String version = conditionContext.
+			getEnvironment()
+			.getProperty("hapi.fhir.fhir_version")
+			.toUpperCase();
+
+		return FhirVersionEnum.R4B.name().equals(version);
+	}
+}
---
+++ src/main/java/ca/uhn/fhir/jpa/starter/common/FhirServerConfigR4B.java
@@ -0,0 +1,17 @@
+package ca.uhn.fhir.jpa.starter.common;
+
+import ca.uhn.fhir.jpa.config.r4b.JpaR4BConfig;
+import ca.uhn.fhir.jpa.starter.annotations.OnR4BCondition;
+import org.springframework.context.annotation.Conditional;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+@Configuration
+@Conditional(OnR4BCondition.class)
+@Import({
+	JpaR4BConfig.class,
+	StarterJpaConfig.class,
+	ElasticsearchConfig.class
+})
+public class FhirServerConfigR4B {
+}
