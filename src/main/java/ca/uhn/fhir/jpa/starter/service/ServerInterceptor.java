e4639d61cfcf596825b5b116c9c1897fcebdd1ef
nsabale7 <nsabale@iprdgroup.com>
2022-12-05 16:16:49 +0530

    removed commented code in ServerInterceptor

--- src/main/java/ca/uhn/fhir/jpa/starter/service/ServerInterceptor.java
+++ src/main/java/ca/uhn/fhir/jpa/starter/service/ServerInterceptor.java
@@ -56,84 +56,84 @@ public class ServerInterceptor {
 	@Hook(Pointcut.STORAGE_PRESTORAGE_RESOURCE_CREATED)
 	public void insert(IBaseResource theResource) throws IOException {
 		notificationDataSource = NotificationDataSource.getInstance();
-//		if(theResource.fhirType().equals("Media")) {
-//			if(((Media) theResource).getContent().hasData()) {
-//				byte[] bitmapdata = ((Media) theResource).getContent().getDataElement().getValue();
-//				String mediaId = ((Media) theResource).getIdElement().getIdPart();
-//				byte[] base64 = Base64.decode(bitmapdata, Base64.DEFAULT);
-//				File image = new File(imagePath+"//"+mediaId+".jpeg");
-//				FileUtils.writeByteArrayToFile(image, base64);
-//				String imagePath = image.getAbsolutePath();
-//				long imageSize = Files.size(Paths.get(imagePath));
-//				long byteSize = base64.length;
-//				if(imageSize == byteSize) {
-//					((Media) theResource).getContent().setDataElement(null);
-//		 			((Media) theResource).getContent().setUrl(imagePath);
-//				}
-//				else {
-//					System.out.println("Image Not Proper");
-//				}	
-//			}
-//		}
-//		else if (theResource.fhirType().equals("Encounter")) {
-//			Encounter encounter = (Encounter) theResource;
-//			String encounterId = encounter.getIdElement().getIdPart();
-//			String patientId = encounter.getSubject().getReferenceElement().getIdPart();
-//			Date currentDate = DateUtilityHelper.getCurrentSqlDate();
-//			String messageStatus = ComGenerator.MessageStatus.PENDING.name();
-//			
-//
-//			ComGenerator comGen = new ComGenerator(
-//				"Encounter",
-//				encounterId,
-//				currentDate,
-//				messageStatus,
-//				patientId,
-//				null
-//			);
-//			
-//			notificationDataSource.insert(comGen);
-//		}
-//		else if(theResource.fhirType().equals("Appointment")) {
-//			Appointment appointment = (Appointment) theResource;
-//			String appointmentId = appointment.getIdElement().getIdPart();
-//			String patientId = appointment.getParticipant().get(0).getActor().getReferenceElement().getIdPart();
-//			
-//			Date currentDate = DateUtilityHelper.getCurrentSqlDate();
-//			Timestamp appointmentScheduledDateTime = DateUtilityHelper.utilDateToTimestamp(appointment.getStart());
-//			String messageStatus = ComGenerator.MessageStatus.PENDING.name();
-//			
-//			ComGenerator comGen = new ComGenerator(
-//					"Appointment",
-//					appointmentId,
-//					currentDate,
-//					messageStatus,
-//					patientId,
-//					appointmentScheduledDateTime
-//				);
-//			
-//			ComGenerator firstReminder = new ComGenerator(
-//					"Appointment",
-//					appointmentId,
-//					DateUtilityHelper.getPreviousDay(appointmentScheduledDateTime),
-//					messageStatus,
-//					patientId,
-//					appointmentScheduledDateTime
-//				);
-//			
-//			ComGenerator secondReminder = new ComGenerator(
-//					"Appointment",
-//					appointmentId,
-//					DateUtilityHelper.timeStampToDate(appointmentScheduledDateTime),
-//					messageStatus,
-//					patientId,
-//					appointmentScheduledDateTime
-//				);
-//			
-//			notificationDataSource.insert(comGen);
-//			notificationDataSource.insert(firstReminder);
-//			notificationDataSource.insert(secondReminder);
-//		}
+		if(theResource.fhirType().equals("Media")) {
+			if(((Media) theResource).getContent().hasData()) {
+				byte[] bitmapdata = ((Media) theResource).getContent().getDataElement().getValue();
+				String mediaId = ((Media) theResource).getIdElement().getIdPart();
+				byte[] base64 = Base64.decode(bitmapdata, Base64.DEFAULT);
+				File image = new File(imagePath+"//"+mediaId+".jpeg");
+				FileUtils.writeByteArrayToFile(image, base64);
+				String imagePath = image.getAbsolutePath();
+				long imageSize = Files.size(Paths.get(imagePath));
+				long byteSize = base64.length;
+				if(imageSize == byteSize) {
+					((Media) theResource).getContent().setDataElement(null);
+		 			((Media) theResource).getContent().setUrl(imagePath);
+				}
+				else {
+					System.out.println("Image Not Proper");
+				}
+			}
+		}
+		else if (theResource.fhirType().equals("Encounter")) {
+			Encounter encounter = (Encounter) theResource;
+			String encounterId = encounter.getIdElement().getIdPart();
+			String patientId = encounter.getSubject().getReferenceElement().getIdPart();
+			Date currentDate = DateUtilityHelper.getCurrentSqlDate();
+			String messageStatus = ComGenerator.MessageStatus.PENDING.name();
+
+
+			ComGenerator comGen = new ComGenerator(
+				"Encounter",
+				encounterId,
+				currentDate,
+				messageStatus,
+				patientId,
+				null
+			);
+
+			notificationDataSource.insert(comGen);
+		}
+		else if(theResource.fhirType().equals("Appointment")) {
+			Appointment appointment = (Appointment) theResource;
+			String appointmentId = appointment.getIdElement().getIdPart();
+			String patientId = appointment.getParticipant().get(0).getActor().getReferenceElement().getIdPart();
+
+			Date currentDate = DateUtilityHelper.getCurrentSqlDate();
+			Timestamp appointmentScheduledDateTime = DateUtilityHelper.utilDateToTimestamp(appointment.getStart());
+			String messageStatus = ComGenerator.MessageStatus.PENDING.name();
+
+			ComGenerator comGen = new ComGenerator(
+					"Appointment",
+					appointmentId,
+					currentDate,
+					messageStatus,
+					patientId,
+					appointmentScheduledDateTime
+				);
+
+			ComGenerator firstReminder = new ComGenerator(
+					"Appointment",
+					appointmentId,
+					DateUtilityHelper.getPreviousDay(appointmentScheduledDateTime),
+					messageStatus,
+					patientId,
+					appointmentScheduledDateTime
+				);
+
+			ComGenerator secondReminder = new ComGenerator(
+					"Appointment",
+					appointmentId,
+					DateUtilityHelper.timeStampToDate(appointmentScheduledDateTime),
+					messageStatus,
+					patientId,
+					appointmentScheduledDateTime
+				);
+
+			notificationDataSource.insert(comGen);
+			notificationDataSource.insert(firstReminder);
+			notificationDataSource.insert(secondReminder);
+		}
 	}
 	
 	@Hook(Pointcut.STORAGE_PRESTORAGE_RESOURCE_UPDATED)
