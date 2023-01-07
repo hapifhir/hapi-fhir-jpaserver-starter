ca0f26e8ad24f550a21d8d2977f66bd7b09cba2c
Jens Kristian Villadsen <jenskristianvilladsen@gmail.com>
2023-01-07 22:55:07 +0100

    Refactored the default resolution

--- src/main/java/ca/uhn/fhir/jpa/starter/ExtraStaticFilesConfigurer.java
+++ src/main/java/ca/uhn/fhir/jpa/starter/ExtraStaticFilesConfigurer.java
@@ -8,23 +8,33 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.net.URI;
+
 @Configuration
 @ConditionalOnProperty(prefix = "hapi.fhir", name = "staticLocation")
 public class ExtraStaticFilesConfigurer implements WebMvcConfigurer {
 
-	@Autowired
-	AppProperties appProperties;
+    public static final String ROOT_CONTEXT_PATH = "/static";
+    @Autowired
+    AppProperties appProperties;
 
-	@Override
-	public void addResourceHandlers(ResourceHandlerRegistry theRegistry) {
-		theRegistry
-			.addResourceHandler("/static/**")
-		.addResourceLocations(appProperties.getStaticLocation());
-	}
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry theRegistry) {
+        theRegistry.addResourceHandler(ROOT_CONTEXT_PATH + "/**").addResourceLocations(appProperties.getStaticLocation());
+    }
 
-	@Override
-	public void addViewControllers(ViewControllerRegistry registry) {
-		registry.addViewController("/static/").setViewName("index.html");
-		registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
-	}
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        String path = URI.create(appProperties.getStaticLocation()).getPath();
+        String lastSegment = path.substring(path.lastIndexOf('/') + 1);
+
+        registry.addViewController(ROOT_CONTEXT_PATH).setViewName("redirect:" + ROOT_CONTEXT_PATH + "/" + lastSegment + "/index.html");
+
+        registry.addViewController(ROOT_CONTEXT_PATH + "/*").setViewName("redirect:" + ROOT_CONTEXT_PATH + "/" + lastSegment + "/index.html");
+
+        registry.addViewController(ROOT_CONTEXT_PATH + "/" + lastSegment + "/").setViewName("redirect:" + ROOT_CONTEXT_PATH + "/" + lastSegment + "/index.html");
+
+        registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
+    }
+
 }
