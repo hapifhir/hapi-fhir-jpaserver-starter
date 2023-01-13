f6416fc380ff8e41161bf6a1b3b51850e6116269
Marc Lang <marc.lang@dedalus.eu>
2023-01-13 14:56:59 +0000

    changing ods to dc4h

--- build-docker-image.sh
+++ build-docker-image.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-docker build -t hapi-fhir/hapi-fhir-jpaserver-ods -t 548749374595.dkr.ecr.eu-west-2.amazonaws.com/hapi-fhir-jpaserver-ods:6.1.0-0.1.0 .
-docker push 548749374595.dkr.ecr.eu-west-2.amazonaws.com/hapi-fhir-jpaserver-ods:6.1.0-0.1.0
+docker build -t hapi-fhir/hapi-fhir-jpaserver-dc4h -t 548749374595.dkr.ecr.eu-west-2.amazonaws.com/hapi-fhir-jpaserver-dc4h:6.1.0-0.1.0 .
+docker push 548749374595.dkr.ecr.eu-west-2.amazonaws.com/hapi-fhir-jpaserver-dc4h:6.1.0-0.1.0
