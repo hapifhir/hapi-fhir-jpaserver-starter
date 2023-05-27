a112a084f8ab4e3907f4a55eae49c0d763d9f3db
Rafael Leite <2132564+leite08@users.noreply.github.com>
2023-05-27 18:30:46 -0500

    build: config to match OSS API + more base DB ACU on staging
    Ref. metriport/metriport-internal#706

--- infra/lib/fhir-server-stack.ts
+++ infra/lib/fhir-server-stack.ts
@@ -15,7 +15,22 @@ import * as sns from "aws-cdk-lib/aws-sns";
 import { ITopic } from "aws-cdk-lib/aws-sns";
 import { Construct } from "constructs";
 import { EnvConfig } from "./env-config";
+import { getConfig } from "./shared/config";
+import { vCPU } from "./shared/fargate";
 import { isProd, mbToBytes } from "./util";
+
+export function settings() {
+  const config = getConfig();
+  const prod = isProd(config);
+  return {
+    cpu: prod ? 2 * vCPU : 1 * vCPU,
+    memoryLimitMiB: prod ? 4096 : 2048,
+    taskCountMin: prod ? 2 : 1,
+    taskCountMax: prod ? 10 : 5,
+    minDBCap: prod ? 4 : 1,
+    maxDBCap: prod ? 32 : 8,
+  };
+}
 
 interface FHIRServerProps extends StackProps {
   config: EnvConfig;
@@ -82,6 +97,8 @@ export class FHIRServerStack extends Stack {
     dbCluster: rds.IDatabaseCluster;
     dbCreds: { username: string; password: secret.Secret };
   } {
+    const { minDBCap, maxDBCap } = settings();
+
     // create database credentials
     const dbClusterName = "fhir-server";
     const dbName = props.config.dbName;
@@ -112,8 +129,6 @@ export class FHIRServerStack extends Stack {
       storageEncrypted: true,
     });
 
-    const minDBCap = this.isProd(props) ? 4 : 0.5;
-    const maxDBCap = this.isProd(props) ? 32 : 4;
     Aspects.of(dbCluster).add({
       visit(node) {
         if (node instanceof rds.CfnDBCluster) {
@@ -140,6 +155,8 @@ export class FHIRServerStack extends Stack {
     dbCreds: { username: string; password: secret.Secret },
     alarmAction?: SnsAction
   ): ecs_patterns.NetworkLoadBalancedFargateService {
+    const { taskCountMin, taskCountMax, cpu, memoryLimitMiB } = settings();
+
     // Create a new Amazon Elastic Container Service (ECS) cluster
     const cluster = new ecs.Cluster(this, "FHIRServerCluster", {
       vpc: this.vpc,
@@ -163,9 +180,9 @@ export class FHIRServerStack extends Stack {
       "FHIRServerFargateService",
       {
         cluster: cluster,
-        cpu: this.isProd(props) ? 2048 : 1024,
-        memoryLimitMiB: this.isProd(props) ? 4096 : 2048,
-        desiredCount: this.isProd(props) ? 3 : 1, // TODO review once we go live
+        cpu,
+        memoryLimitMiB,
+        desiredCount: taskCountMin,
         taskImageOptions: {
           image: ecs.ContainerImage.fromDockerImageAsset(dockerImage),
           containerPort: 8080,
@@ -231,8 +248,8 @@ export class FHIRServerStack extends Stack {
 
     // hookup autoscaling based on 90% thresholds
     const scaling = fargateService.service.autoScaleTaskCount({
-      minCapacity: this.isProd(props) ? 2 : 1,
-      maxCapacity: this.isProd(props) ? 10 : 2,
+      minCapacity: taskCountMin,
+      maxCapacity: taskCountMax,
     });
     scaling.scaleOnCpuUtilization("autoscale_cpu", {
       targetUtilizationPercent: 90,
