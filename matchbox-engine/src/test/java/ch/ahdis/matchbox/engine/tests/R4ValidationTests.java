3692f45ef591c43348ade506652a34574d0a16ff
Quentin Ligier <quentin.ligier@bluewin.ch>
2023-07-05 12:58:52 +0200

    Add R4 resources validation tests

---
+++ matchbox-engine/src/test/java/ch/ahdis/matchbox/engine/tests/R4ValidationTests.java
@@ -0,0 +1,118 @@
+package ch.ahdis.matchbox.engine.tests;
+
+import ch.ahdis.matchbox.engine.MatchboxEngine;
+import org.hl7.fhir.exceptions.FHIRException;
+import org.hl7.fhir.r4.model.OperationOutcome;
+import org.hl7.fhir.r5.elementmodel.Manager;
+import org.hl7.fhir.r5.utils.EOperationOutcome;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * A test bench to test the validation of R4 resources.
+ *
+ * @author Quentin Ligier
+ **/
+class R4ValidationTests {
+	private final MatchboxEngine engine;
+	private final String careplanRaw;
+	private final String measureRaw;
+
+	public R4ValidationTests() throws IOException, URISyntaxException {
+		this.engine = this.getEngine();
+		this.careplanRaw = this.loadSample("careplan.xml");
+		this.measureRaw = this.loadSample("measure.xml");
+	}
+
+	/**
+	 * Test the validation of a code from a value set defined with simple includes.
+	 *
+	 * http://hl7.org/fhir/R4/careplan.html
+	 * http://hl7.org/fhir/R4/valueset-care-plan-intent.html
+	 */
+	@Test
+	void testValueSetWithSimpleInclude() throws Exception {
+		final String validCareplan = this.careplanRaw.replace("{{INTENT}}", "plan");
+		this.expectValid(validCareplan, Manager.FhirFormat.XML, "http://hl7.org/fhir/StructureDefinition/CarePlan");
+
+		final String invalidCareplan = this.careplanRaw.replace("{{INTENT}}", "non-existent-code");
+		final var errors = this.expectInvalid(invalidCareplan, Manager.FhirFormat.XML, "http://hl7" +
+			".org/fhir/StructureDefinition/CarePlan");
+		assertEquals(1, errors.size());
+		assertEquals(OperationOutcome.IssueType.CODEINVALID, errors.get(0).getCode());
+		assertTrue(errors.get(0).getDetails().getText().startsWith("The value provided ('non-existent-code') is not in the value set 'Care Plan Intent' (http://hl7.org/fhir/ValueSet/care-plan-intent|4.0.1)"));
+	}
+
+	/**
+	 * Test the validation of a code from a value set defined with all the codes from a code system.
+	 *
+	 * http://hl7.org/fhir/R4/measure.html
+	 * http://hl7.org/fhir/R4/valueset-publication-status.html
+	 */
+	@Test
+	void testValueSetWithAllFromCodeSystem() throws Exception {
+		final String validMeasure = this.measureRaw.replace("{{STATUS}}", "active");
+		this.expectValid(validMeasure, Manager.FhirFormat.XML, "http://hl7.org/fhir/StructureDefinition/Measure");
+
+		final String invalidMeasure = this.measureRaw.replace("{{STATUS}}", "non-existent-code");
+		final var errors = this.expectInvalid(invalidMeasure, Manager.FhirFormat.XML, "http://hl7.org/fhir/StructureDefinition/Measure");
+		assertEquals(1, errors.size());
+		assertEquals(OperationOutcome.IssueType.CODEINVALID, errors.get(0).getCode());
+		assertTrue(errors.get(0).getDetails().getText().startsWith("The value provided ('non-existent-code') is not in the value set 'PublicationStatus'"));
+	}
+
+	/**
+	 * Todo: test the validation of a code from a value set defined with 'concept is-a' filter from SNOMED CT.
+	 */
+
+	/**
+	 * Todo: test the validation of a code from a value set defined with 'canonical =' filter from UCUM.
+	 */
+
+	List<OperationOutcome.OperationOutcomeIssueComponent> getValidationFailures(final OperationOutcome outcome) {
+		return outcome.getIssue().stream()
+			.filter(issue -> OperationOutcome.IssueSeverity.FATAL == issue.getSeverity() || OperationOutcome.IssueSeverity.ERROR == issue.getSeverity())
+			.collect(Collectors.toList());
+	}
+
+	/**
+	 * Initialize a R4 matchbox engine with no terminology server.
+	 */
+	private MatchboxEngine getEngine() throws IOException, URISyntaxException {
+		return new MatchboxEngine.MatchboxEngineBuilder().getEngineR4();
+	}
+
+	private void expectValid(final String resource,
+									 final Manager.FhirFormat format,
+									 final String profile) throws EOperationOutcome, IOException {
+		final var response = this.engine.validate(new ByteArrayInputStream(resource.getBytes(StandardCharsets.UTF_8)),
+																format,
+																profile);
+		assertEquals(0, getValidationFailures(response).size());
+	}
+
+	private List<OperationOutcome.OperationOutcomeIssueComponent> expectInvalid(final String resource,
+																										 final Manager.FhirFormat format,
+																										 final String profile) throws EOperationOutcome, IOException {
+		final var response = this.engine.validate(new ByteArrayInputStream(resource.getBytes(StandardCharsets.UTF_8)),
+																format,
+																profile);
+		final var errors = getValidationFailures(response);
+		assertNotEquals(0, errors.size());
+		return errors;
+	}
+
+	private String loadSample(final String filename) throws IOException {
+		return new String(
+			R4ValidationTests.class.getResourceAsStream("/r4-samples/" + filename).readAllBytes()
+		);
+	}
+}
