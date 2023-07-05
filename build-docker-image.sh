5d41a23ce5f9289a350ec328ecb905c42a603376
Jolyon Pawlyn <jpawlyn@gmail.com>
2023-07-05 12:55:27 +0200

    Add mtomady logo and docker build instructions

--- build-docker-image.sh
+++ build-docker-image.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
-docker build -t hapi-fhir/hapi-fhir-jpaserver-starter .
+# to build for multiple platforms see https://docs.docker.com/build/building/multi-platform/#getting-started
+# specifically docker buildx create --name mybuilder --driver docker-container --bootstrap --use
+docker buildx build --platform linux/amd64,linux/arm64 -t mtomady/hapi --push .
 
