6e745a911227de5baf79ba0dbd8827c0ad131e43
Quentin Ligier <quentin.ligier@bluewin.ch>
2023-07-19 09:52:30 +0200

    Reformat files

--- matchbox-server/src/main/java/ch/ahdis/fhir/hapi/jpa/validation/ValidationProvider.java
+++ matchbox-server/src/main/java/ch/ahdis/fhir/hapi/jpa/validation/ValidationProvider.java
@@ -1,15 +1,5 @@
 package ch.ahdis.fhir.hapi.jpa.validation;
 
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
-
 /*
  * #%L
  * Matchbox Server
@@ -19,9 +9,9 @@ import java.util.stream.Collectors;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -29,8 +19,19 @@ import java.util.stream.Collectors;
  * limitations under the License.
  * #L%
  */
-import javax.servlet.http.HttpServletRequest;
 
+import ca.uhn.fhir.context.FhirContext;
+import ca.uhn.fhir.rest.annotation.Operation;
+import ca.uhn.fhir.rest.annotation.OperationParam;
+import ca.uhn.fhir.rest.api.EncodingEnum;
+import ca.uhn.fhir.util.StopWatch;
+import ca.uhn.fhir.validation.ResultSeverityEnum;
+import ca.uhn.fhir.validation.SingleValidationMessage;
+import ca.uhn.fhir.validation.ValidationResult;
+import ch.ahdis.matchbox.CliContext;
+import ch.ahdis.matchbox.MatchboxEngineSupport;
+import ch.ahdis.matchbox.engine.MatchboxEngine;
+import ch.ahdis.matchbox.engine.cli.VersionUtil;
 import org.apache.commons.beanutils.BeanUtils;
 import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.commons.io.IOUtils;
@@ -44,43 +45,38 @@ import org.hl7.fhir.r4.model.StructureDefinition;
 import org.hl7.fhir.r5.elementmodel.Manager.FhirFormat;
 import org.hl7.fhir.r5.utils.EOperationOutcome;
 import org.hl7.fhir.utilities.validation.ValidationMessage;
-import ch.ahdis.matchbox.CliContext;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-import ca.uhn.fhir.context.FhirContext;
-import ca.uhn.fhir.rest.annotation.Operation;
-import ca.uhn.fhir.rest.annotation.OperationParam;
-import ca.uhn.fhir.rest.api.EncodingEnum;
-import ca.uhn.fhir.util.StopWatch;
-import ca.uhn.fhir.validation.ResultSeverityEnum;
-import ca.uhn.fhir.validation.SingleValidationMessage;
-import ca.uhn.fhir.validation.ValidationResult;
-import ch.ahdis.matchbox.MatchboxEngineSupport;
-import ch.ahdis.matchbox.engine.MatchboxEngine;
-import ch.ahdis.matchbox.engine.cli.VersionUtil;
+import javax.servlet.http.HttpServletRequest;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Operation $validate
  */
 public class ValidationProvider {
 
+	// @Autowired
+	// protected DefaultProfileValidationSupport defaultProfileValidationSuport;
+	/*
+	 * @Autowired
+	 *
+	 * @Qualifier("myJpaValidationSupport") protected IValidationSupport
+	 * myJpaValidationSupport;
+	 */
+	private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(ValidationProvider.class);
+
 	@Autowired
 	protected MatchboxEngineSupport matchboxEngineSupport;
 
 	@Autowired
 	protected CliContext cliContext;
-
-	// @Autowired
-	// protected DefaultProfileValidationSupport defaultProfileValidationSuport;
-	/*
-	 * @Autowired
-	 * 
-	 * @Qualifier("myJpaValidationSupport") protected IValidationSupport
-	 * myJpaValidationSupport;
-	 */
-	private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(ValidationProvider.class);
 
 //	@Operation(name = "$canonical", manualRequest = true, idempotent = true, returnParameters = {
 //			@OperationParam(name = "return", type = IBase.class, min = 1, max = 1) })
@@ -103,27 +99,27 @@ public class ValidationProvider {
 //	}
 
 	@Operation(name = "$validate", manualRequest = true, idempotent = true, returnParameters = {
-			@OperationParam(name = "return", type = IBase.class, min = 1, max = 1) })
-	public IBaseResource validate(HttpServletRequest theRequest) {
+		@OperationParam(name = "return", type = IBase.class, min = 1, max = 1)})
+	public IBaseResource validate(final HttpServletRequest theRequest) {
 
 		// FIXME, we need to do this version independent
 		// we should be able to extract the version from the 
-		// a) the request int version is set int the request
-		// b) ig parameter is specified whichis fixed to a FHIR Version
+		// a) the request int version is set in the request
+		// b) ig parameter is specified which is fixed to a FHIR Version
 		// c) profile parameter is specified and we can resolve it to a FHIR version
-		FhirContext myFhirCtx = FhirContext.forR4Cached();
+		final FhirContext myFhirCtx = FhirContext.forR4Cached();
 
 		log.info("$validate");
-		ArrayList<SingleValidationMessage> addedValidationMessages = new ArrayList<>();
+		final ArrayList<SingleValidationMessage> addedValidationMessages = new ArrayList<>();
 
-		StopWatch sw = new StopWatch();
+		final StopWatch sw = new StopWatch();
 		sw.startTask("Total");
 
 		String profile = null;
 		boolean reload = false;
 
 		// we extract here all config
-		CliContext cliContext = new CliContext(this.cliContext);
+		final CliContext cliContext = new CliContext(this.cliContext);
 
 		// get al list of all JsonProperty of cliContext with return values property name and property type
 		List<Field> cliContextProperties = cliContext.getValidateEngineParameters();
@@ -141,7 +137,8 @@ public class ValidationProvider {
 						BeanUtils.setProperty(cliContext, cliContextProperty, value);
 					}
 				} catch (IllegalAccessException | InvocationTargetException e) {
-					log.error("error setting property " + cliContextProperty + " to " + theRequest.getParameter(cliContextProperty));
+					log.error("error setting property " + cliContextProperty + " to " + theRequest.getParameter(
+						cliContextProperty));
 				}
 			}
 		}
@@ -162,9 +159,10 @@ public class ValidationProvider {
 		if (engine.getStructureDefinition(profile) == null) {
 			return getValidationMessageProfileNotSupported(profile);
 		}
-		
+
 		if (!matchboxEngineSupport.isInitialized()) {
-			OperationOutcomeIssueComponent a = new OperationOutcomeIssueComponent().setDiagnostics("validation engine not initialized, please try again").setSeverity(IssueSeverity.ERROR);
+			OperationOutcomeIssueComponent a = new OperationOutcomeIssueComponent().setDiagnostics(
+				"validation engine not initialized, please try again").setSeverity(IssueSeverity.ERROR);
 			return new OperationOutcome().addIssue(a);
 		}
 
@@ -190,19 +188,21 @@ public class ValidationProvider {
 			encoding = EncodingEnum.detectEncoding(contentString);
 		}
 
-		ValidationResult result = null;
-		result = validateWithResult(engine, contentString, encoding, profile);
+		final ValidationResult result = validateWithResult(engine, contentString, encoding, profile);
 		return getOperationOutcome(sha3Hex, addedValidationMessages, sw, structDef, result, engine, cliContext);
 	}
 
-	private ValidationResult validateWithResult(MatchboxEngine engine, String contentString, EncodingEnum encoding, String profile) {
+	private ValidationResult validateWithResult(final MatchboxEngine engine,
+															  final String contentString,
+															  final EncodingEnum encoding,
+															  final String profile) {
 		try {
-			ArrayList<SingleValidationMessage> hapiMessages = new ArrayList<SingleValidationMessage>();
-			List<ValidationMessage> messages = engine.validate(
-					(encoding == EncodingEnum.XML ? FhirFormat.XML : FhirFormat.JSON),
-					new ByteArrayInputStream(contentString.getBytes("UTF-8")), profile);
+			final ArrayList<SingleValidationMessage> hapiMessages = new ArrayList<>();
+			final List<ValidationMessage> messages = engine.validate(
+				(encoding == EncodingEnum.XML ? FhirFormat.XML : FhirFormat.JSON),
+				new ByteArrayInputStream(contentString.getBytes(StandardCharsets.UTF_8)), profile);
 
-			for (ValidationMessage riMessage : messages) {
+			for (final ValidationMessage riMessage : messages) {
 				SingleValidationMessage hapiMessage = new SingleValidationMessage();
 				if (riMessage.getCol() != -1) {
 					hapiMessage.setLocationCol(riMessage.getCol());
@@ -215,11 +215,10 @@ public class ValidationProvider {
 
 				// MATCHBOX added
 				String message = riMessage.getMessage();
-				ValidationMessage vm = riMessage;
-				if (vm != null && vm.sliceText != null) {
+				if (riMessage.sliceText != null) {
 					message += " Slice info:";
-					for (int i = 0; i < vm.sliceText.length; ++i) {
-						String s = vm.sliceText[i];
+					for (int i = 0; i < riMessage.sliceText.length; ++i) {
+						String s = riMessage.sliceText[i];
 						message += " " + (i + 1) + ".) " + s;
 					}
 				}
@@ -233,20 +232,17 @@ public class ValidationProvider {
 				// theCtx.addValidationMessage(hapiMessage);
 				hapiMessages.add(hapiMessage);
 			}
-			ValidationResult result = new ValidationResult(FhirContext.forR4Cached(), hapiMessages);
-			return result;
-		} catch (FHIRException e) {
-		} catch (UnsupportedEncodingException e) {
-		} catch (IOException e) {
-		} catch (EOperationOutcome e) {
+			return new ValidationResult(FhirContext.forR4Cached(), hapiMessages);
+		} catch (final Exception e) {
+			log.error(e.getMessage(), e);
 		}
 		return null;
 	}
 
 //
 
-	private String getContentString(HttpServletRequest theRequest,
-			ArrayList<SingleValidationMessage> addedValidationMessages) {
+	private String getContentString(final HttpServletRequest theRequest,
+											  final ArrayList<SingleValidationMessage> addedValidationMessages) {
 		byte[] bytes = null;
 		String contentString = "";
 		try {
@@ -259,30 +255,36 @@ public class ValidationProvider {
 					SingleValidationMessage m = new SingleValidationMessage();
 					m.setSeverity(ResultSeverityEnum.WARNING);
 					m.setMessage(
-							"Resource content has a UTF-8 BOM marking, skipping BOM, see https://en.wikipedia.org/wiki/Byte_order_mark");
+						"Resource content has a UTF-8 BOM marking, skipping BOM, see https://en.wikipedia.org/wiki/Byte_order_mark");
 					m.setLocationCol(0);
 					m.setLocationLine(0);
 					addedValidationMessages.add(m);
 				}
 			}
 			contentString = new String(bytes);
-		} catch (IOException e) {
+		} catch (final IOException e) {
+			log.error(e.getMessage(), e);
 		}
 		return contentString;
 	}
 
-	private IBaseResource getOperationOutcome(String id, ArrayList<SingleValidationMessage> addedValidationMessages,
-			StopWatch sw, StructureDefinition profile, ValidationResult result, MatchboxEngine engine, CliContext cli) {
+	private IBaseResource getOperationOutcome(final String id,
+															final ArrayList<SingleValidationMessage> addedValidationMessages,
+															final StopWatch sw,
+															final StructureDefinition profile,
+															final ValidationResult result,
+															final MatchboxEngine engine,
+															final CliContext cli) {
 		sw.endCurrentTask();
 
 		FhirContext myFhirCtx = FhirContext.forR4Cached();
 
-		log.info("Validation time: " + sw.toString());
-		
+		log.info("Validation time: " + sw);
+
 		String packages = "with packages: ";
 		List<String> pkgs = engine.getContext().getLoadedPackages();
-		for(int i=0; i<pkgs.size(); ++i) {
-			if (i>0) {
+		for (int i = 0; i < pkgs.size(); ++i) {
+			if (i > 0) {
 				packages += ", ";
 			}
 			packages += pkgs.get(i);
@@ -291,14 +293,14 @@ public class ValidationProvider {
 		SingleValidationMessage m = new SingleValidationMessage();
 		m.setSeverity(ResultSeverityEnum.INFORMATION);
 		m.setMessage("Validation "
-				+ (profile != null
-						? "for profile " + profile.getUrl() + "|" + profile.getVersion() + " "
-								+ (profile.getDateElement() != null ? "(" + profile.getDateElement().asStringValue() + ") " : " ")
-						: "") +packages + " " 
-				+ (result.getMessages().size() == 0 ? "No Issues detected. " : "") + sw.formatTaskDurations() + " "
-				+ VersionUtil.getPoweredBy()
-				+ " validation parameters "+cli.toString());
-	 	
+							 + (profile != null
+			? "for profile " + profile.getUrl() + "|" + profile.getVersion() + " "
+			+ (profile.getDateElement() != null ? "(" + profile.getDateElement().asStringValue() + ") " : " ")
+			: "") + packages + " "
+							 + (result.getMessages().size() == 0 ? "No Issues detected. " : "") + sw.formatTaskDurations() + " "
+							 + VersionUtil.getPoweredBy()
+							 + " validation parameters " + cli.toString());
+
 		m.setLocationCol(0);
 		m.setLocationLine(0);
 		addedValidationMessages.add(m);
@@ -306,7 +308,7 @@ public class ValidationProvider {
 		addedValidationMessages.addAll(result.getMessages());
 
 		IBaseResource operationOutcome = new ValidationResultWithExtensions(myFhirCtx, addedValidationMessages)
-				.toOperationOutcome();
+			.toOperationOutcome();
 		operationOutcome.setId(id);
 
 		log.info(myFhirCtx.newXmlParser().encodeResourceToString(operationOutcome));
@@ -314,14 +316,14 @@ public class ValidationProvider {
 		return operationOutcome;
 	}
 
-	private IBaseResource getValidationMessageProfileNotSupported(String profile) {
+	private IBaseResource getValidationMessageProfileNotSupported(final String profile) {
 		SingleValidationMessage m = new SingleValidationMessage();
 		m.setSeverity(ResultSeverityEnum.ERROR);
 		m.setMessage("Validation for profile " + profile
-				+ " not supported by this server, but additional ig's could be configured.");
+							 + " not supported by this server, but additional ig's could be configured.");
 		m.setLocationCol(0);
 		m.setLocationLine(0);
-		ArrayList<SingleValidationMessage> newValidationMessages = new ArrayList<>();
+		final ArrayList<SingleValidationMessage> newValidationMessages = new ArrayList<>(1);
 		newValidationMessages.add(m);
 		return (new ValidationResult(FhirContext.forR4Cached(), newValidationMessages)).toOperationOutcome();
 	}
--- matchbox-server/src/main/java/ch/ahdis/fhir/hapi/jpa/validation/ValidationResultWithExtensions.java
+++ matchbox-server/src/main/java/ch/ahdis/fhir/hapi/jpa/validation/ValidationResultWithExtensions.java
@@ -1,14 +1,5 @@
 package ch.ahdis.fhir.hapi.jpa.validation;
 
-import static org.apache.commons.lang3.StringUtils.isNotBlank;
-
-import java.util.List;
-
-import org.hl7.fhir.instance.model.api.IBase;
-import org.hl7.fhir.instance.model.api.IBaseExtension;
-import org.hl7.fhir.instance.model.api.IBaseOperationOutcome;
-import org.hl7.fhir.instance.model.api.IPrimitiveType;
-
 import ca.uhn.fhir.context.BaseRuntimeChildDefinition;
 import ca.uhn.fhir.context.BaseRuntimeElementCompositeDefinition;
 import ca.uhn.fhir.context.FhirContext;
@@ -16,12 +7,21 @@ import ca.uhn.fhir.rest.api.Constants;
 import ca.uhn.fhir.util.OperationOutcomeUtil;
 import ca.uhn.fhir.validation.SingleValidationMessage;
 import ca.uhn.fhir.validation.ValidationResult;
+import org.hl7.fhir.instance.model.api.IBase;
+import org.hl7.fhir.instance.model.api.IBaseExtension;
+import org.hl7.fhir.instance.model.api.IBaseOperationOutcome;
+import org.hl7.fhir.instance.model.api.IPrimitiveType;
 
-public class ValidationResultWithExtensions extends ca.uhn.fhir.validation.ValidationResult {
+import java.util.List;
 
-  private final List<SingleValidationMessage> myMessages;
-  private final FhirContext myCtx;
-  
+import static org.apache.commons.lang3.StringUtils.isNotBlank;
+
+public class ValidationResultWithExtensions extends ValidationResult {
+
+	private final List<SingleValidationMessage> myMessages;
+
+	private final FhirContext myCtx;
+
 //  public static void addLocationToIssue(FhirContext theContext, IBase theIssue, String theLocation) {
 //    if (isNotBlank(theLocation)) {
 //      BaseRuntimeElementCompositeDefinition<?> issueElement = (BaseRuntimeElementCompositeDefinition<?>) theContext.getElementDefinition(theIssue.getClass());
@@ -33,64 +33,65 @@ public class ValidationResultWithExtensions extends ca.uhn.fhir.validation.Valid
 //  }
 
 
-  
-  public void addLocationLineToIssue(FhirContext theContext, IBase theIssue, int theLocation) {
-    BaseRuntimeElementCompositeDefinition<?> issueElement = (BaseRuntimeElementCompositeDefinition<?>) theContext.getElementDefinition(theIssue.getClass());
-    BaseRuntimeChildDefinition extensionChild = issueElement.getChildByName("extension");
+	public ValidationResultWithExtensions(FhirContext theCtx, List<SingleValidationMessage> theMessages) {
+		super(theCtx, theMessages);
+		myCtx = theCtx;
+		myMessages = theMessages;
+	}
 
-    IBaseExtension<?,?> extensionElem = (IBaseExtension<?,?>) extensionChild.getChildByName("extension").newInstance(extensionChild.getInstanceConstructorArguments());
-    extensionElem.setUrl("http://hl7.org/fhir/StructureDefinition/operationoutcome-issue-line");
-    
-    IPrimitiveType<Integer> value = (IPrimitiveType<Integer>) theContext.getElementDefinition("integer").newInstance();
-    value.setValue(theLocation);
-    extensionElem.setValue(value);
-    
-    extensionChild.getMutator().addValue(theIssue, extensionElem);
-  }
+	public void addLocationLineToIssue(FhirContext theContext, IBase theIssue, int theLocation) {
+		BaseRuntimeElementCompositeDefinition<?> issueElement = (BaseRuntimeElementCompositeDefinition<?>) theContext.getElementDefinition(
+			theIssue.getClass());
+		BaseRuntimeChildDefinition extensionChild = issueElement.getChildByName("extension");
 
-  @Override
-  public void populateOperationOutcome(IBaseOperationOutcome theOperationOutcome) {
-    for (SingleValidationMessage next : myMessages) {
-      String location;
-      if (isNotBlank(next.getLocationString())) {
-        location = next.getLocationString();
-      } else if (next.getLocationLine() != null || next.getLocationCol() != null) {
-        location = "Line[" + next.getLocationLine() + "] Col[" + next.getLocationCol() + "]";
-      } else {
-        location = null;
-      }
-      String severity = next.getSeverity() != null ? next.getSeverity().getCode() : null;
-      IBase issue = OperationOutcomeUtil.addIssue(myCtx, theOperationOutcome, severity, next.getMessage(), location,
-          Constants.OO_INFOSTATUS_PROCESSING);
+		IBaseExtension<?, ?> extensionElem = (IBaseExtension<?, ?>) extensionChild.getChildByName("extension").newInstance(
+			extensionChild.getInstanceConstructorArguments());
+		extensionElem.setUrl("http://hl7.org/fhir/StructureDefinition/operationoutcome-issue-line");
 
-      if (next.getLocationLine() != null || next.getLocationCol() != null) {
-        String unknown = "(unknown)";
-        String line = unknown;
-        if (next.getLocationLine() != null && next.getLocationLine() != -1) {
-          this.addLocationLineToIssue(myCtx, issue, next.getLocationLine());
-          line = next.getLocationLine().toString();
-        }
-        String col = unknown;
-        if (next.getLocationCol() != null && next.getLocationCol() != -1) {
-          col = next.getLocationCol().toString();
-        }
-        if (!unknown.equals(line) || !unknown.equals(col)) {
-          OperationOutcomeUtil.addLocationToIssue(myCtx, issue, "Line " + line + ", Col " + col);
-        }
-      }
-    }
+		IPrimitiveType<Integer> value = (IPrimitiveType<Integer>) theContext.getElementDefinition("integer").newInstance();
+		value.setValue(theLocation);
+		extensionElem.setValue(value);
 
-    if (myMessages.isEmpty()) {
-      String message = myCtx.getLocalizer().getMessage(ValidationResult.class, "noIssuesDetected");
-      OperationOutcomeUtil.addIssue(myCtx, theOperationOutcome, "information", message, null, "informational");
-    }
-  }
+		extensionChild.getMutator().addValue(theIssue, extensionElem);
+	}
 
-  public ValidationResultWithExtensions(FhirContext theCtx, List<SingleValidationMessage> theMessages) {
-    super(theCtx, theMessages);
-    myCtx = theCtx;
-    myMessages = theMessages;
-  }
+	@Override
+	public void populateOperationOutcome(IBaseOperationOutcome theOperationOutcome) {
+		for (SingleValidationMessage next : myMessages) {
+			String location;
+			if (isNotBlank(next.getLocationString())) {
+				location = next.getLocationString();
+			} else if (next.getLocationLine() != null || next.getLocationCol() != null) {
+				location = "Line[" + next.getLocationLine() + "] Col[" + next.getLocationCol() + "]";
+			} else {
+				location = null;
+			}
+			String severity = next.getSeverity() != null ? next.getSeverity().getCode() : null;
+			IBase issue = OperationOutcomeUtil.addIssue(myCtx, theOperationOutcome, severity, next.getMessage(), location,
+																	  Constants.OO_INFOSTATUS_PROCESSING);
+
+			if (next.getLocationLine() != null || next.getLocationCol() != null) {
+				String unknown = "(unknown)";
+				String line = unknown;
+				if (next.getLocationLine() != null && next.getLocationLine() != -1) {
+					this.addLocationLineToIssue(myCtx, issue, next.getLocationLine());
+					line = next.getLocationLine().toString();
+				}
+				String col = unknown;
+				if (next.getLocationCol() != null && next.getLocationCol() != -1) {
+					col = next.getLocationCol().toString();
+				}
+				if (!unknown.equals(line) || !unknown.equals(col)) {
+					OperationOutcomeUtil.addLocationToIssue(myCtx, issue, "Line " + line + ", Col " + col);
+				}
+			}
+		}
+
+		if (myMessages.isEmpty()) {
+			String message = myCtx.getLocalizer().getMessage(ValidationResult.class, "noIssuesDetected");
+			OperationOutcomeUtil.addIssue(myCtx, theOperationOutcome, "information", message, null, "informational");
+		}
+	}
 
 
 }
