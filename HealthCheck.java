e42b30736f0186b8b6676dd53ae0b6e552b2c441
Kashyap Jois <kjois@iprdgroup.com>
2023-08-25 18:03:04 +0530

    Add import statement

--- HealthCheck.java
+++ HealthCheck.java
@@ -3,6 +3,8 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse.BodyHandlers;
+import java.time.Duration;
+
 public class HealthCheck {
   public static void main(String[] args) throws InterruptedException, IOException {
     var client = HttpClient.newHttpClient();
