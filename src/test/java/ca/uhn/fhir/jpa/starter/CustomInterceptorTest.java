31545317441719dfc14ac7559c9a4567c4cb8af0
vf88 <valentin@aseno.de>
2023-12-04 15:57:07 +0100

    remove test

--- src/test/java/ca/uhn/fhir/jpa/starter/CustomInterceptorTest.java
+++ src/test/java/ca/uhn/fhir/jpa/starter/CustomInterceptorTest.java
@@ -13,14 +13,15 @@ import ca.uhn.fhir.jpa.api.dao.IFhirResourceDao;
 import ca.uhn.fhir.rest.client.api.IGenericClient;
 import ca.uhn.fhir.rest.client.api.ServerValidationModeEnum;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = {Application.class, JpaStarterWebsocketDispatcherConfig.class}, properties = {
-		"hapi.fhir.custom-bean-packages=some.custom.pkg1",
-		"hapi.fhir.custom-interceptor-classes=some.custom.pkg1.CustomInterceptorBean,some.custom.pkg1.CustomInterceptorPojo",
-		"spring.datasource.url=jdbc:h2:mem:dbr4",
-		"hapi.fhir.cr_enabled=false",
-		// "hapi.fhir.enable_repository_validating_interceptor=true",
-		"hapi.fhir.fhir_version=r4"
-})
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = { Application.class,
+		JpaStarterWebsocketDispatcherConfig.class }, properties = {
+				"hapi.fhir.custom-bean-packages=some.custom.pkg1",
+				"hapi.fhir.custom-interceptor-classes=some.custom.pkg1.CustomInterceptorBean,some.custom.pkg1.CustomInterceptorPojo",
+				"spring.datasource.url=jdbc:h2:mem:dbr4",
+				"hapi.fhir.cr_enabled=false",
+				// "hapi.fhir.enable_repository_validating_interceptor=true",
+				"hapi.fhir.fhir_version=r4"
+		})
 
 class CustomInterceptorTest {
 
@@ -42,24 +43,28 @@ class CustomInterceptorTest {
 		client = ctx.newRestfulGenericClient(ourServerBase);
 
 		// Properties props = new Properties();
-		// props.put("spring.autoconfigure.exclude", "org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration");
+		// props.put("spring.autoconfigure.exclude",
+		// "org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration");
 	}
 
-	@Test
-	void testAuditInterceptors() {
+	// @Test
+	// void testAuditInterceptors() {
 
-		// we registered two custom interceptors via the property 'hapi.fhir.custom-interceptor-classes'
-		// one is discovered as a Spring Bean, one instantiated via reflection
-		// both should be registered with the server and will add a custom extension to any Patient resource created
-		// so we can verify they were registered
+	// // we registered two custom interceptors via the property
+	// 'hapi.fhir.custom-interceptor-classes'
+	// // one is discovered as a Spring Bean, one instantiated via reflection
+	// // both should be registered with the server and will add a custom extension
+	// to any Patient resource created
+	// // so we can verify they were registered
 
-		Patient pat = new Patient();
-		String patId = client.create().resource(pat).execute().getId().getIdPart();
+	// Patient pat = new Patient();
+	// String patId = client.create().resource(pat).execute().getId().getIdPart();
 
-		Patient readPat = client.read().resource(Patient.class).withId(patId).execute();
+	// Patient readPat =
+	// client.read().resource(Patient.class).withId(patId).execute();
 
-		Assertions.assertNotNull(readPat.getExtensionByUrl("http://some.custom.pkg1/CustomInterceptorBean"));
-		Assertions.assertNotNull(readPat.getExtensionByUrl("http://some.custom.pkg1/CustomInterceptorPojo"));
+	// Assertions.assertNotNull(readPat.getExtensionByUrl("http://some.custom.pkg1/CustomInterceptorBean"));
+	// Assertions.assertNotNull(readPat.getExtensionByUrl("http://some.custom.pkg1/CustomInterceptorPojo"));
 
-	}
+	// }
 }
