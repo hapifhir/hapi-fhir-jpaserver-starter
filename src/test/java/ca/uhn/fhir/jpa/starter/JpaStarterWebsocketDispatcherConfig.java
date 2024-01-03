0a95e71cbc26b46f38a4df56e7f1e342fc690ac7
Matteo Steccolini <stmsat@gmail.com>
2024-01-03 15:12:38 +0100

    fixed integration test execution; tests are now done with tomcat, unless "jetty" profile is active (in which case they're done with jetty)
    Test class JpaStarterWebsocketDispatcherConfig emptied, not sure if necessary (it's now missing jetty dependencies)

--- src/test/java/ca/uhn/fhir/jpa/starter/JpaStarterWebsocketDispatcherConfig.java
+++ src/test/java/ca/uhn/fhir/jpa/starter/JpaStarterWebsocketDispatcherConfig.java
@@ -1,10 +1,10 @@
 package ca.uhn.fhir.jpa.starter;
 
-import org.eclipse.jetty.ee10.webapp.WebAppContext;
-import org.eclipse.jetty.ee10.websocket.jakarta.server.config.JakartaWebSocketServletContainerInitializer;
-import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
-import org.springframework.boot.web.server.WebServerFactoryCustomizer;
-import org.springframework.context.annotation.Bean;
+//import org.eclipse.jetty.ee10.webapp.WebAppContext;
+//import org.eclipse.jetty.ee10.websocket.jakarta.server.config.JakartaWebSocketServletContainerInitializer;
+//import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
+//import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+//import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
@@ -14,21 +14,21 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class JpaStarterWebsocketDispatcherConfig {
 
-	@Bean
-	public Jetty12WebSocketServletWebServerCustomizer jetty10WebSocketServletWebServerCustomizer() {
-		return new Jetty12WebSocketServletWebServerCustomizer();
-	}
-
-	static class Jetty12WebSocketServletWebServerCustomizer implements WebServerFactoryCustomizer<JettyServletWebServerFactory> {
-
-		@Override
-		public void customize(JettyServletWebServerFactory factory) {
-
-			factory.addServerCustomizers(server -> {
-				WebAppContext ctx = (WebAppContext) server.getHandler();
-				JakartaWebSocketServletContainerInitializer.configure(ctx, null);
-			});
-
-		}
-	}
+//	@Bean
+//	public Jetty12WebSocketServletWebServerCustomizer jetty10WebSocketServletWebServerCustomizer() {
+//		return new Jetty12WebSocketServletWebServerCustomizer();
+//	}
+//
+//	static class Jetty12WebSocketServletWebServerCustomizer implements WebServerFactoryCustomizer<JettyServletWebServerFactory> {
+//
+//		@Override
+//		public void customize(JettyServletWebServerFactory factory) {
+//
+//			factory.addServerCustomizers(server -> {
+//				WebAppContext ctx = (WebAppContext) server.getHandler();
+//				JakartaWebSocketServletContainerInitializer.configure(ctx, null);
+//			});
+//
+//		}
+//	}
 }
