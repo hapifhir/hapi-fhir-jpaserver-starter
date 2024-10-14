91f34fab2282e0b7d9f5327cafa63a4248c58431
Quentin Ligier <quentin.ligier@bluewin.ch>
2024-10-14 10:08:12 +0200

    Rebuild the frontend

---
+++ matchbox-server/src/main/resources/static/browser/chunk-2D2MSWRU.js
@@ -0,0 +1,1 @@
+import{Ic as a,Jc as b}from"./chunk-PO3V6XUW.js";import"./chunk-TSRGIXR5.js";export{b as HighlightLineNumbers,a as activateLineNumbers};
--- matchbox-server/src/main/resources/static/browser/chunk-F54VN76F.js
+++
@@ -1,1 +0,0 @@
-import{Ic as a,Jc as b}from"./chunk-R3KSG7OY.js";import"./chunk-TSRGIXR5.js";export{b as HighlightLineNumbers,a as activateLineNumbers};
