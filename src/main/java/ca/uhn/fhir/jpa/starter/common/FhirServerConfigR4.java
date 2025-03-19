9cf166e765415c8cb185ab0ac031a404c7a27618
Jens Kristian Villadsen <jenskristianvilladsen@gmail.com>
2025-03-19 13:46:51 +0100

    Added support for hybrid remote terminology

--- src/main/java/ca/uhn/fhir/jpa/starter/common/FhirServerConfigR4.java
+++ src/main/java/ca/uhn/fhir/jpa/starter/common/FhirServerConfigR4.java
@@ -1,9 +1,18 @@
 package ca.uhn.fhir.jpa.starter.common;
 
+import ca.uhn.fhir.context.FhirContext;
+import ca.uhn.fhir.context.support.ConceptValidationOptions;
+import ca.uhn.fhir.context.support.IValidationSupport;
+import ca.uhn.fhir.context.support.ValidationSupportContext;
 import ca.uhn.fhir.jpa.config.r4.JpaR4Config;
+import ca.uhn.fhir.jpa.starter.AppProperties;
 import ca.uhn.fhir.jpa.starter.annotations.OnR4Condition;
+import ca.uhn.fhir.jpa.starter.common.validation.OnRemoteTerminologyPresent;
 import ca.uhn.fhir.jpa.starter.cr.StarterCrR4Config;
 import ca.uhn.fhir.jpa.starter.ips.StarterIpsConfig;
+import org.hl7.fhir.common.hapi.validation.support.RemoteTerminologyServiceValidationSupport;
+import org.hl7.fhir.common.hapi.validation.support.ValidationSupportChain;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -17,4 +26,24 @@ import org.springframework.context.annotation.Import;
 	ElasticsearchConfig.class,
 	StarterIpsConfig.class
 })
-public class FhirServerConfigR4 {}
+public class FhirServerConfigR4 {
+
+	@Bean(name = "myHybridRemoteValidationSupportChain")
+	@Conditional({OnR4Condition.class, OnRemoteTerminologyPresent.class})
+	public IValidationSupport addRemoteValidation(ValidationSupportChain theValidationSupport, FhirContext theFhirContext, AppProperties theAppProperties)
+	{
+		theAppProperties.getRemoteTerminologyServicesMap().forEach((key, remoteSystem) -> {
+			theValidationSupport.addValidationSupport(0 , new RemoteTerminologyServiceValidationSupport(theFhirContext, remoteSystem.getUrl()){
+				@Override
+				public CodeValidationResult validateCode(ValidationSupportContext theValidationSupportContext, ConceptValidationOptions theOptions, String theCodeSystem, String theCode, String theDisplay, String theValueSetUrl) {
+					if (remoteSystem.getSystem().equalsIgnoreCase(theCodeSystem)) {
+						return super.validateCode(theValidationSupportContext, theOptions, theCodeSystem, theCode, theDisplay, theValueSetUrl);
+					}
+					return null;
+				}
+			});
+		});
+
+		return theValidationSupport;
+	}
+}
