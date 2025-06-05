0e75261b1e6ea9f19adcb62e6962a6392635cbca
Jens Kristian Villadsen <jenskristianvilladsen@gmail.com>
2025-06-05 23:04:45 +0200

    Reverting dialect change - fit for a potential other PR

--- src/main/java/ca/uhn/fhir/jpa/starter/util/JpaHibernatePropertiesProvider.java
+++ src/main/java/ca/uhn/fhir/jpa/starter/util/JpaHibernatePropertiesProvider.java
@@ -5,8 +5,6 @@ import ca.uhn.fhir.jpa.config.HibernatePropertiesProvider;
 import org.hibernate.dialect.Dialect;
 import org.hibernate.engine.jdbc.dialect.internal.StandardDialectResolver;
 import org.hibernate.engine.jdbc.dialect.spi.DatabaseMetaDataDialectResolutionInfoAdapter;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 
 import java.sql.Connection;
@@ -15,32 +13,16 @@ import javax.sql.DataSource;
 
 public class JpaHibernatePropertiesProvider extends HibernatePropertiesProvider {
 
-	private static final Logger logger = LoggerFactory.getLogger(JpaHibernatePropertiesProvider.class);
-	public static final String PACKAGE_NAME = "ca.uhn.fhir.jpa.model.dialect.HapiFhir";
-
 	private final Dialect dialect;
 
 	public JpaHibernatePropertiesProvider(LocalContainerEntityManagerFactoryBean myEntityManagerFactory) {
 		DataSource connection = myEntityManagerFactory.getDataSource();
-		Dialect temporaryDialect;
-		try {
-			assert connection != null;
-			try (Connection dbConnection = connection.getConnection()) {
-				temporaryDialect = new StandardDialectResolver()
-						.resolveDialect(new DatabaseMetaDataDialectResolutionInfoAdapter(dbConnection.getMetaData()));
-			}
+		try (Connection dbConnection = connection.getConnection()) {
+			dialect = new StandardDialectResolver()
+					.resolveDialect(new DatabaseMetaDataDialectResolutionInfoAdapter(dbConnection.getMetaData()));
 		} catch (SQLException sqlException) {
 			throw new ConfigurationException(sqlException.getMessage(), sqlException);
 		}
-		try {
-			temporaryDialect = (Dialect) Class.forName(
-							PACKAGE_NAME.concat(temporaryDialect.getClass().getSimpleName()))
-					.getDeclaredConstructor()
-					.newInstance();
-		} catch (Exception e) {
-			logger.error("Looking up optimized HAPI adjusted dialect failed", e);
-		}
-		dialect = temporaryDialect;
 	}
 
 	@Override
