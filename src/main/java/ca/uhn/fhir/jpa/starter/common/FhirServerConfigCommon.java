7cd0637c88b0f8d8d8679d0dbf31bfa27e6c7bf2
Michal Sevcik <99325737+SevcikMichal@users.noreply.github.com>
2025-06-08 11:40:26 +0200

    Enable configuration of store_meta_source_information (#825)
    * Enable configuration of store_meta_source_information
    
    * Update src/main/java/ca/uhn/fhir/jpa/starter/common/FhirServerConfigCommon.java
    
    Co-authored-by: James Agnew <jamesagnew@gmail.com>
    
    * Apply spotless style checks
    
    ---------
    
    Co-authored-by: James Agnew <jamesagnew@gmail.com>

--- src/main/java/ca/uhn/fhir/jpa/starter/common/FhirServerConfigCommon.java
+++ src/main/java/ca/uhn/fhir/jpa/starter/common/FhirServerConfigCommon.java
@@ -254,6 +254,10 @@ public class FhirServerConfigCommon {
 		jpaStorageSettings.setBundleBatchPoolSize(appProperties.getBundle_batch_pool_size());
 		jpaStorageSettings.setBundleBatchPoolSize(appProperties.getBundle_batch_pool_max_size());
 
+		// Set store meta source information
+		ourLog.debug("Server configured to Store Meta Source: {}", appProperties.getStore_meta_source_information());
+		jpaStorageSettings.setStoreMetaSourceInformation(appProperties.getStore_meta_source_information());
+
 		storageSettings(appProperties, jpaStorageSettings);
 		return jpaStorageSettings;
 	}
