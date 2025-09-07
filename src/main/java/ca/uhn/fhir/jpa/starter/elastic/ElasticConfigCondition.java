e61f6a2b7d23144f52416c73eaa0c9e6199e1ac8
Jens Kristian Villadsen <jenskristianvilladsen@gmail.com>
2025-09-07 22:52:54 +0200

    corrected condition

--- src/main/java/ca/uhn/fhir/jpa/starter/elastic/ElasticConfigCondition.java
+++ src/main/java/ca/uhn/fhir/jpa/starter/elastic/ElasticConfigCondition.java
@@ -1,3 +1,19 @@
 package ca.uhn.fhir.jpa.starter.elastic;
 
-public class ElasticConfigCondition {}
+import org.springframework.boot.autoconfigure.elasticsearch.ElasticsearchProperties;
+import org.springframework.boot.context.properties.bind.Binder;
+import org.springframework.context.annotation.Condition;
+import org.springframework.context.annotation.ConditionContext;
+import org.springframework.core.type.AnnotatedTypeMetadata;
+
+public class ElasticConfigCondition implements Condition {
+
+	@Override
+	public boolean matches(ConditionContext theConditionContext, AnnotatedTypeMetadata theAnnotatedTypeMetadata) {
+		ElasticsearchProperties config = Binder.get(theConditionContext.getEnvironment())
+				.bind("spring.elasticsearch", ElasticsearchProperties.class)
+				.orElse(null);
+		if (config == null) return false;
+		else return true;
+	}
+}
