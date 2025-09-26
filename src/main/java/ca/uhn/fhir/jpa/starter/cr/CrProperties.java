3673c135b10f2e9eb3f4937780b93cab50e5ed81
Brenin Rhodes <brenin.rhodes@smiledigitalhealth.com>
2025-09-26 09:27:13 -0600

    Update to 3.27.0 release

--- src/main/java/ca/uhn/fhir/jpa/starter/cr/CrProperties.java
+++ src/main/java/ca/uhn/fhir/jpa/starter/cr/CrProperties.java
@@ -11,7 +11,7 @@ public class CrProperties {
 
 	private CareGapsProperties careGaps = new CareGapsProperties();
 	private CqlProperties cql = new CqlProperties();
-	private TerminologyServerClientSettings terminologyServerClientSettings = new TerminologyServerClientSettings();
+	private TerminologyServerClientSettings terminologyServerClientSettings = TerminologyServerClientSettings.getDefault();
 
 	public Boolean getEnabled() {
 		return enabled;
