37913d7148137971be3274e3a62267be4be3ff5b
Ahmet Melih Aydoğdu <melih.aydogdu@formelio.nl>
2025-10-31 16:24:17 +0100

    resource creation updated

--- src/main/java/nl/hinq/NviInterceptor.java
+++ src/main/java/nl/hinq/NviInterceptor.java
@@ -7,6 +7,8 @@ import ca.uhn.fhir.rest.api.server.RequestDetails;
 import ca.uhn.fhir.rest.client.api.IGenericClient;
 import ca.uhn.fhir.rest.client.api.ServerValidationModeEnum;
 import lombok.extern.slf4j.Slf4j;
+
+import org.apache.jena.atlas.lib.InternalErrorException;
 import org.hl7.fhir.instance.model.api.IBaseResource;
 import org.hl7.fhir.r4.model.*;
 
@@ -17,11 +19,7 @@ public class NviInterceptor {
 
 	private static final String URA_SYSTEM = "http://fhir.nl/fhir/NamingSystem/ura";
 	private static final String BSN_SYSTEM = "http://fhir.nl/fhir/NamingSystem/bsn";
-	private static final String DOCUMENT_REFERENCE_PROFILE = "http://nuts-foundation.github.io/nl-generic-functions-ig/StructureDefinition/nl-gf-localization-documentreference";
-	private static final String DOCUMENT_REFERENCE_DATA = "This DocumentReference does not point to a specific document, but rather to documents or data in general at the custodian referenced in this instance. This can be used by data localization services (also known as medical record localization services).";
-	// In practice this would be configured in properties
 	private static final String ORG_URA = "90000697";
-	private static final String ORG_TYPE = "Z3";
 	private static final String NVI_ENDPOINT = "http://dev-nuts-hackaton-source-nuts-knooppunt.dev-nuts-hackathon-source.svc.cluster.local:8081/nvi";
 	private static final String MITZ_ENDPOINT = "http://dev-nuts-hackaton-source-nuts-knooppunt.dev-nuts-hackathon-source.svc.cluster.local:8081/mitz";
 
@@ -39,17 +37,17 @@ public class NviInterceptor {
 				return;
 			}
 
-			DocumentReference documentReference = createNviEntry(optionalBsn.get());
+			String documentReference = DOCUMENT_REFERENCE.formatted(optionalBsn.get(), ORG_URA);
 			postDocumentReference(requestDetails, documentReference);
 			log.info("Posted DocumentReference to NVI for patient with ID: {}", newResource.getIdElement().getIdPart());
 
-			Subscription subscription = createMitzSubscription(optionalBsn.get());
+			String subscription = SUBSCRIPTION.formatted(optionalBsn.get(), ORG_URA);
 			postSubscription(requestDetails, subscription);
 			log.info("Posted Subscription to Mitz for patient with ID: {}", newResource.getIdElement().getIdPart());
 		}
 	}
 
-	private void postDocumentReference(RequestDetails requestDetails, DocumentReference docRef) {
+	private void postDocumentReference(RequestDetails requestDetails, String docRef) {
 		FhirContext context = requestDetails.getFhirContext();
 		var factory = context.getRestfulClientFactory();
 		factory.setServerValidationMode(ServerValidationModeEnum.NEVER);
@@ -58,49 +56,16 @@ public class NviInterceptor {
 			client.create()
 				.resource(docRef)
 				.withAdditionalHeader("X-Tenant-Id", URA_SYSTEM+"|"+ORG_URA)
+				.withAdditionalHeader("Content-Type", "application/fhir+json")
 				.execute();
 		} catch (Exception e) {
-			log.error("Error posting DocumentReference to NVI: {}", e.getMessage());
-			throw e;
+			String message = "Error posting DocumentReference to NVI: %s".formatted(e.getMessage());
+			log.error(message);
+			throw new InternalErrorException(message);
 		}
 	}
 
-	private DocumentReference createNviEntry(String bsn) {
-		DocumentReference docRef = new DocumentReference();
-		// Add profile
-		docRef.getMeta().addProfile(DOCUMENT_REFERENCE_PROFILE);
-		// Add BSN
-		Identifier bsnIdentifier = new Identifier();
-		bsnIdentifier.setSystem(BSN_SYSTEM);
-		bsnIdentifier.setValue(bsn);
-		docRef.addIdentifier(bsnIdentifier);
-		// Set status
-		docRef.setStatus(Enumerations.DocumentReferenceStatus.CURRENT);
-		// Add type
-		Coding typeCoding = new Coding();
-		typeCoding.setSystem("http://loinc.org");
-		typeCoding.setCode("55188-7");
-		typeCoding.setDisplay("Patient data Document");
-		CodeableConcept type = new CodeableConcept();
-		type.addCoding(typeCoding);
-		docRef.setType(type);
-		// Set custodian
-		Identifier uraIdentifier = new Identifier();
-		uraIdentifier.setSystem(URA_SYSTEM);
-		uraIdentifier.setValue(ORG_URA);
-		Reference custodianReference = new Reference();
-		custodianReference.setIdentifier(uraIdentifier);
-		docRef.setCustodian(custodianReference);
-		// Set content
-		Attachment attachment = new Attachment();
-		attachment.setContentType("text/plain");
-		attachment.setData(java.util.Base64.getEncoder().encode(DOCUMENT_REFERENCE_DATA.getBytes()));
-		attachment.setTitle("Generic reference to patient data");
-		docRef.addContent().setAttachment(attachment);
-		return docRef;
-	}
-
-	private void postSubscription(RequestDetails requestDetails, Subscription subscription) {
+	private void postSubscription(RequestDetails requestDetails, String subscription) {
 		FhirContext context = requestDetails.getFhirContext();
 		var factory = context.getRestfulClientFactory();
 		factory.setServerValidationMode(ServerValidationModeEnum.NEVER);
@@ -108,25 +73,63 @@ public class NviInterceptor {
 		try {
 			client.create()
 				.resource(subscription)
+				.withAdditionalHeader("Content-Type", "application/fhir+json")
 				.execute();
 		} catch (Exception e) {
-			log.error("Error posting Subscription to Mitz: {}", e.getMessage());
-			throw e;
+			String message = "Error posting Subscription to Mitz: %s".formatted(e.getMessage());
+			log.error(message);
+			throw new InternalErrorException(message);
 		}
 	}
 
-	private Subscription createMitzSubscription(String bsn) {
-		Subscription subscription = new Subscription();
-		subscription.setStatus(Subscription.SubscriptionStatus.REQUESTED);
-		subscription.setReason("OTV");
-		// Set criteria to match DocumentReference for the given BSN
-		subscription.setCriteria(String.format("Consent?_query=otv&patientid=%s&providerid=%s&providertype=%s",
-			bsn, ORG_URA, ORG_TYPE));
-		// Set channel
-		Subscription.SubscriptionChannelComponent channel = new Subscription.SubscriptionChannelComponent();
-		channel.setType(Subscription.SubscriptionChannelType.RESTHOOK);
-		channel.setPayload("application/fhir+json");
-		subscription.setChannel(channel);
-		return subscription;
-	}
+	private static String DOCUMENT_REFERENCE = "{\n" + //
+				"  \"resourceType\" : \"DocumentReference\",\n" + //
+				"  \"meta\" : {\n" + //
+				"    \"profile\" : [\n" + //
+				"      \"http://nuts-foundation.github.io/nl-generic-functions-ig/StructureDefinition/nl-gf-localization-documentreference\"\n" + //
+				"    ]\n" + //
+				"  },\n" + //
+				"  \"status\" : \"current\",\n" + //
+				"  \"type\" : {\n" + //
+				"    \"coding\" : [\n" + //
+				"      {\n" + //
+				"        \"system\" : \"http://loinc.org\",\n" + //
+				"        \"code\" : \"55188-7\",\n" + //
+				"        \"display\" : \"Patient data Document\"\n" + //
+				"      }\n" + //
+				"    ]\n" + //
+				"  },\n" + //
+				"  \"subject\" : {\n" + //
+				"    \"identifier\" : {\n" + //
+				"      \"system\" : \"http://fhir.nl/fhir/NamingSystem/bsn\",\n" + //
+				"      \"value\" : \"%s\"\n" + //
+				"    }\n" + //
+				"  },\n" + //
+				"  \"custodian\" : {\n" + //
+				"    \"identifier\" : {\n" + //
+				"      \"system\" : \"http://fhir.nl/fhir/NamingSystem/ura\",\n" + //
+				"      \"value\" : \"%s\"\n" + //
+				"    }\n" + //
+				"  },\n" + //
+				"  \"content\" : [\n" + //
+				"    {\n" + //
+				"      \"attachment\" : {\n" + //
+				"        \"contentType\" : \"text/plain\",\n" + //
+				"        \"data\" : \"IlRoaXMgRG9jdW1lbnRSZWZlcmVuY2UgZG9lcyBub3QgcG9pbnQgdG8gYSBzcGVjaWZpYyBkb2N1bWVudCwgYnV0IHJhdGhlciB0byBkb2N1bWVudHMgb3IgZGF0YSBpbiBnZW5lcmFsIGF0IHRoZSBjdXN0b2RpYW4gcmVmZXJlbmNlZCBpbiB0aGlzIGluc3RhbmNlLiBUaGlzIGNhbiBiZSB1c2VkIGJ5IGRhdGEgbG9jYWxpemF0aW9uIHNlcnZpY2VzIChhbHNvIGtub3duIGFzIG1lZGljYWwgcmVjb3JkIGxvY2FsaXphdGlvbiBzZXJ2aWNlcykuIg==\",\n" + //
+				"        \"title\" : \"Generic reference to patient data\"\n" + //
+				"      }\n" + //
+				"    }\n" + //
+				"  ]\n" + //
+				"}";
+
+	private static String SUBSCRIPTION = "{\n" + //
+				"    \"resourceType\": \"Subscription\",\n" + //
+				"    \"status\": \"requested\",\n" + //
+				"    \"reason\": \"OTV\",\n" + //
+				"    \"criteria\": \"Consent?_query=otv&patientid=%s&providerid=%s&providertype=Z3\",\n" + //
+				"    \"channel\": {\n" + //
+				"        \"type\": \"rest-hook\",\n" + //
+				"        \"payload\": \"application/fhir+json\"\n" + //
+				"    }\n" + //
+				"}";
 }
