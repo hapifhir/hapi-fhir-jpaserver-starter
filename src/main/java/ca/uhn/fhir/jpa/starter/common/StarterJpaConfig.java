bf21bdd4ed0153ade7f095ca83fa98ca849382de
Jens Kristian Villadsen <jenskristianvilladsen@gmail.com>
2025-11-25 20:29:25 +0100

    Making separate profile for ES

--- src/main/java/ca/uhn/fhir/jpa/starter/common/StarterJpaConfig.java
+++ src/main/java/ca/uhn/fhir/jpa/starter/common/StarterJpaConfig.java
@@ -150,7 +150,7 @@ public class StarterJpaConfig {
 	@Primary
 	@Bean
 	public LocalContainerEntityManagerFactoryBean entityManagerFactory(
-			ElasticsearchBootSvcImpl elasticsearchSvc,
+			Optional<ElasticsearchBootSvcImpl> elasticsearchSvc,
 			JpaProperties theJpaProperties,
 			DataSource myDataSource,
 			ConfigurableListableBeanFactory myConfigurableListableBeanFactory,
