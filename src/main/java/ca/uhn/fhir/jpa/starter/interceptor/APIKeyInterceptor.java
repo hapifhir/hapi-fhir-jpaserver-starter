b3537a5e0379c08b3fb74b7a9b7f4332b770474f
Martin Smock <martin_smock@me.com>
2025-12-07 09:03:49 +0100

    added interceptor on top of HAPI authorization framework
    The interceptor allows write operations only if a X-API-KEY header is present and matches one configured in application.yaml

--- src/main/java/ca/uhn/fhir/jpa/starter/interceptor/APIKeyInterceptor.java
+++ src/main/java/ca/uhn/fhir/jpa/starter/interceptor/APIKeyInterceptor.java
@@ -1,28 +1,60 @@
 package ca.uhn.fhir.jpa.starter.interceptor;
 
-import ca.uhn.fhir.interceptor.api.Hook;
+import ca.uhn.fhir.i18n.Msg;
 import ca.uhn.fhir.interceptor.api.Interceptor;
-import ca.uhn.fhir.interceptor.api.Pointcut;
 import ca.uhn.fhir.rest.api.server.RequestDetails;
+import ca.uhn.fhir.rest.server.exceptions.AuthenticationException;
+import ca.uhn.fhir.rest.server.interceptor.auth.AuthorizationInterceptor;
+import ca.uhn.fhir.rest.server.interceptor.auth.IAuthRule;
+import ca.uhn.fhir.rest.server.interceptor.auth.RuleBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Set;
 
 /**
  * Interceptor class for handling API key-related tasks during incoming server requests.
- *
+ * <p>
  * Registered using the {@link @Interceptor} annotation, this class hooks into the
  * server's request-processing lifecycle at the defined pointcut.
  */
+@Component
 @Interceptor
-public class APIKeyInterceptor {
+public class APIKeyInterceptor extends AuthorizationInterceptor {
 
-	private final Logger ourLog = LoggerFactory.getLogger(APIKeyInterceptor.class);
+	private final Logger logger = LoggerFactory.getLogger(APIKeyInterceptor.class);
 
-	@Hook(Pointcut.SERVER_INCOMING_REQUEST_POST_PROCESSED)
-	public boolean logRequests(RequestDetails theRequest) {
+	@Value("${security.api-keys}")
+	private Set<String> registeredKeys;
 
-		ourLog.info("Request of type {} with request ID: {}", theRequest.getRequestType(), theRequest.getRequestId());
+	@Override
+	public List<IAuthRule> buildRuleList(RequestDetails theRequestDetails) {
 
-		return true; // if false, the request is blocked and the connection is closed
+		// In this basic example we have hardcoded API Keys.
+		boolean userIsAdmin = false;
+		String apiKey = theRequestDetails.getHeader("X-API-KEY");
+		logger.info("API Key from header is {}.", apiKey);
+
+		if (apiKey != null && registeredKeys.contains(apiKey)) {
+			logger.info("The api key matches one of the keys registered in application.yaml. Unrestricted access will be granted.");
+			return new RuleBuilder().allowAll().build();
+		}
+
+		if (apiKey == null || !registeredKeys.contains(apiKey)) {
+
+			logger.info("API key is null or not registered. Only read access will be granted.");
+
+			return new RuleBuilder()
+				.allow()
+				.read()
+				.allResources()
+				.withAnyId()
+				.build();
+		}
+
+		throw new AuthenticationException(Msg.code(644) + "Missing or invalid X-API-KEY header value");
 	}
 }
