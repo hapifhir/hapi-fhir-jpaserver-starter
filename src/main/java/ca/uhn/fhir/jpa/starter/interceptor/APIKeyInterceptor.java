6421e8fd71f8c7d4ef51f4aa9224d2837f9c22df
Joel Bach <6702422+joel-bach@users.noreply.github.com>
2025-12-12 08:11:27 +0100

    allow terminology server operations

--- src/main/java/ca/uhn/fhir/jpa/starter/interceptor/APIKeyInterceptor.java
+++ src/main/java/ca/uhn/fhir/jpa/starter/interceptor/APIKeyInterceptor.java
@@ -6,7 +6,12 @@ import ca.uhn.fhir.rest.api.server.RequestDetails;
 import ca.uhn.fhir.rest.server.exceptions.AuthenticationException;
 import ca.uhn.fhir.rest.server.interceptor.auth.AuthorizationInterceptor;
 import ca.uhn.fhir.rest.server.interceptor.auth.IAuthRule;
+import ca.uhn.fhir.rest.server.interceptor.auth.IAuthRuleBuilder;
 import ca.uhn.fhir.rest.server.interceptor.auth.RuleBuilder;
+import org.hl7.fhir.instance.model.api.IBaseResource;
+import org.hl7.fhir.r4.model.CodeSystem;
+import org.hl7.fhir.r4.model.ConceptMap;
+import org.hl7.fhir.r4.model.ValueSet;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -35,25 +40,38 @@ public class APIKeyInterceptor extends AuthorizationInterceptor {
 
 		// In this basic example we have API Keys configured in application.yaml.
 		String apiKey = theRequestDetails.getHeader("X-API-KEY");
-		logger.info("API Key from header is {}.", apiKey);
 
 		if (apiKey != null && registeredKeys.contains(apiKey)) {
-			logger.info("The api key matches one of the keys registered in application.yaml. Unrestricted access will be granted.");
+			logger.debug("The api key matches one of the keys registered in application.yaml. Unrestricted access will be granted.");
 			return new RuleBuilder().allowAll().build();
 		}
 
 		if (apiKey == null || !registeredKeys.contains(apiKey)) {
 
-			logger.info("API key is null or not registered. Only read access will be granted.");
+			logger.debug("API key is null or not registered. Only read access will be granted.");
 
-			return new RuleBuilder()
-				.allow()
-				.read()
-				.allResources()
-				.withAnyId()
-				.build();
+			var builder = new RuleBuilder();
+			builder.allow().metadata();
+			allowReadAndOperationsOnResourceType(builder, CodeSystem.class, List.of("lookup", "validate-code", "subsumes"));
+			allowReadAndOperationsOnResourceType(builder, ValueSet.class, List.of("expand", "validate-code"));
+			allowReadAndOperationsOnResourceType(builder, ConceptMap.class, List.of("translate"));
+			return builder.build();
 		}
 
 		throw new AuthenticationException(Msg.code(644) + "Missing or invalid X-API-KEY header value");
+	}
+
+	private void allowReadAndOperationsOnResourceType(IAuthRuleBuilder builder, Class<? extends IBaseResource> type, List<String> operations) {
+		builder
+			.allow()
+			.read()
+			.resourcesOfType(type)
+			.withAnyId();
+		for (String operation : operations) {
+			builder.allow()
+				.operation().named(operation)
+				.onType(type)
+				.andAllowAllResponses();
+		}
 	}
 }
