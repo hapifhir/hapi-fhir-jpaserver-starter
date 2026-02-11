5002435625ab0e88a09c9b4530a4390667fe88f6
qligier <3299399+qligier@users.noreply.github.com>
2026-02-11 18:36:40 +0000

    server: rebuild the Angular GUI resources

--- matchbox-server/src/main/resources/static/browser/chunk-MUJYNG5S.js
+++
@@ -1,1 +0,0 @@
-import{Tc as a,Uc as b}from"./chunk-Z3WZWENC.js";import"./chunk-TSRGIXR5.js";export{b as HighlightLineNumbers,a as activateLineNumbers};
---
+++ matchbox-server/src/main/resources/static/browser/chunk-XPMQEOBF.js
@@ -0,0 +1,1 @@
+import{Xc as a,Yc as b}from"./chunk-IFMSMYLP.js";import"./chunk-TSRGIXR5.js";export{b as HighlightLineNumbers,a as activateLineNumbers};
