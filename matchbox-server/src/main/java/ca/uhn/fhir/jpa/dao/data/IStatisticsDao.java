9fa08855889c150a0321edf811b647c2c83f7442
Flaewel <flavio.bachmann@hotmail.com>
2026-02-16 08:58:53 +0100

    Used StatisticsEntity instead of copied NpmPackageVersionResourceEntity. Removed unused import.

--- matchbox-server/src/main/java/ca/uhn/fhir/jpa/dao/data/IStatisticsDao.java
+++ matchbox-server/src/main/java/ca/uhn/fhir/jpa/dao/data/IStatisticsDao.java
@@ -1,10 +1,10 @@
 package ca.uhn.fhir.jpa.dao.data;
 
-import ca.uhn.fhir.jpa.model.entity.NpmPackageVersionResourceEntity;
+import ca.uhn.fhir.jpa.model.entity.StatisticsEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface IStatisticsDao extends JpaRepository<NpmPackageVersionResourceEntity, Long>, IHapiFhirJpaRepository {
+public interface IStatisticsDao extends JpaRepository<StatisticsEntity, Long>, IHapiFhirJpaRepository {
   
 }
