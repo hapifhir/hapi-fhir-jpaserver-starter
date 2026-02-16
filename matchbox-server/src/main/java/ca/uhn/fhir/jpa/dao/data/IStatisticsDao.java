b24d239f0a274b957ad71a9054e98868804c680e
Flaewel <flavio.bachmann@hotmail.com>
2026-02-16 11:05:06 +0100

    Added ConditionalOnProperty to check for save-statistics in application.yaml

--- matchbox-server/src/main/java/ca/uhn/fhir/jpa/dao/data/IStatisticsDao.java
+++ matchbox-server/src/main/java/ca/uhn/fhir/jpa/dao/data/IStatisticsDao.java
@@ -1,10 +1,12 @@
 package ca.uhn.fhir.jpa.dao.data;
 
 import ca.uhn.fhir.jpa.model.entity.StatisticsEntity;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@ConditionalOnProperty(prefix = "matchbox.validation", name = "save-statistics", havingValue = "true")
 public interface IStatisticsDao extends JpaRepository<StatisticsEntity, Long>, IHapiFhirJpaRepository {
   
 }
