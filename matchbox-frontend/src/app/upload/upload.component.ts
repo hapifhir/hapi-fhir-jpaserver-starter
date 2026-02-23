d3816db63de2e01e2d96ea195412de9fcab72efa
Quentin Ligier <quentin.ligier@bluewin.ch>
2026-02-23 20:58:38 +0100

    Save selected file in UploadComponent

--- matchbox-frontend/src/app/upload/upload.component.ts
+++ matchbox-frontend/src/app/upload/upload.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, EventEmitter, Output } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
 
 export interface IDroppedBlob {
   blob: File;
@@ -7,22 +7,21 @@ export interface IDroppedBlob {
 }
 
 @Component({
-    selector: 'app-upload',
-    templateUrl: './upload.component.html',
-    styleUrls: ['./upload.component.scss'],
-    changeDetection: ChangeDetectionStrategy.OnPush,
-    standalone: false
+  selector: 'app-upload',
+  templateUrl: './upload.component.html',
+  styleUrls: ['./upload.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  standalone: false,
 })
 export class UploadComponent {
   @Output() addFiles = new EventEmitter<IDroppedBlob>();
 
   dragCounter = 0;
 
-  checkStatus(response): boolean {
-    if (!response.ok) {
-      throw new Error(`HTTP ${response.status} - ${response.statusText}`);
-    }
-    return response;
+  @Output() selectedFile: IDroppedBlob | null = null;
+
+  @Input() clear() {
+    this.selectedFile = null;
   }
 
   onDrop(ev) {
@@ -31,15 +30,14 @@ export class UploadComponent {
     this.dragCounter = 0;
     const files = ev.target.files || ev.dataTransfer.items;
     if (files) {
-      for (let i = 0; i < files.length; i++) {
+      for (const element of files) {
         // If dropped items aren't files, reject them
-        if (files[i].kind === undefined || files[i].kind === 'file') {
-          const file = files[i].getAsFile ? files[i].getAsFile() : files[i];
-          this.addFiles.emit({ name: file.name, contentType: '', blob: file });
+        if (element.kind === undefined || element.kind === 'file') {
+          const file = element.getAsFile ? element.getAsFile() : element;
+          this.setSelectedFile({ name: file.name, contentType: '', blob: file });
         }
-        if (files[i].kind === 'string' || files[i].kind === 'text/uri-list') {
-          var url = ev.dataTransfer.getData('URL');
-          this.fetchData(url);
+        if (element.kind === 'string' || element.kind === 'text/uri-list') {
+          this.fetchData(ev.dataTransfer.getData('URL'));
         }
       }
     }
@@ -48,8 +46,13 @@ export class UploadComponent {
   async fetchData(url: string) {
     const res = await fetch(url, { cache: 'no-store' });
     const contentType = res.headers.get('Content-Type');
-    const blob = await res.blob() as File;
-    this.addFiles.emit({ name: url, contentType, blob });
+    const blob = (await res.blob()) as File;
+    this.setSelectedFile({ name: url, contentType, blob });
+  }
+
+  setSelectedFile(file: IDroppedBlob) {
+    this.addFiles.emit(file);
+    this.selectedFile = file;
   }
 
   onDragOver(ev: DragEvent) {
