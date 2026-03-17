a8ecf7573254aaf30942a8aefa39c0261f9c62c9
Flaewel <flavio.bachmann@hotmail.com>
2026-03-17 16:34:13 +0100

    Added search parameters (not yet functional) and full operation outcome display when one is selected. Tweaked some styles

--- matchbox-frontend/src/app/statistics/statistics.component.ts
+++ matchbox-frontend/src/app/statistics/statistics.component.ts
@@ -1,16 +1,23 @@
 import { AfterViewInit, Component } from '@angular/core';
 import FhirClient from 'fhir-kit-client'
 import { FhirConfigService } from '../fhirConfig.service';
+import { FormControl } from '@angular/forms';
+
+
 
 @Component({
   selector: 'app-statistics',
-  imports: [],
   templateUrl: './statistics.component.html',
   styleUrl: './statistics.component.scss',
+  standalone: false
 })
 export class StatisticsComponent implements AfterViewInit {
   operationOutcomes: any[] = [];
   client: FhirClient;
+  selectedOutcomeId: number | null  = null;
+  filterStartDate = new FormControl<Date | null>(null);
+  filterEndDate = new FormControl<Date | null>(null);
+  filterSelectedSeverity = new FormControl<string[]>([], { nonNullable: true});
 
   constructor(private data: FhirConfigService) {
     this.client = data.getFhirClient();
@@ -25,6 +32,22 @@ export class StatisticsComponent implements AfterViewInit {
       }
         
     })
+    
+  }
+
+  selectRow(id: number) {
+    if (this.selectedOutcomeId === id) {
+      this.selectedOutcomeId = null;
+    } else {
+      this.selectedOutcomeId = id;
+    }
+    console.log('Selected OO ID: ', id);
+  }
+
+  filterOutcomes() {
+    const startDate = this.filterStartDate.value;
+    const endDate = this.filterEndDate.value;
+    const selectedSeverities = this.filterSelectedSeverity.value;
     
   }
 
