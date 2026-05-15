3e2d1ef5a79373ba6a5b4ebd3c73eb905cfcd433
c-schuler <hoofschu@gmail.com>
2026-05-15 10:00:25 -0600

    Fix for measureEvaluationOptions bean collision; Added H2 database files to .gitignore

--- src/test/java/ca/uhn/fhir/jpa/starter/MeasureEvaluationConfig.java
+++ src/test/java/ca/uhn/fhir/jpa/starter/MeasureEvaluationConfig.java
@@ -1,10 +1,12 @@
 package ca.uhn.fhir.jpa.starter;
 
 import org.opencds.cqf.fhir.cr.measure.MeasureEvaluationOptions;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ConditionalOnProperty(name = "hapi.fhir.cr.enabled", havingValue = "false", matchIfMissing = true)
 public class MeasureEvaluationConfig {
 
 	@Bean
