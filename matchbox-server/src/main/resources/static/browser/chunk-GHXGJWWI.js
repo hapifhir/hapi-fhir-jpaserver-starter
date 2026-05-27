3008d4e2768d3feb646e2347b6b9774a9928e9d3
Quentin Ligier <quentin.ligier@bluewin.ch>
2026-05-27 21:23:52 +0200

    update frontend dependencies

---
+++ matchbox-server/src/main/resources/static/browser/chunk-GHXGJWWI.js
@@ -0,0 +1,1 @@
+import{Zc as a,_c as b}from"./chunk-PRE56HRG.js";import"./chunk-TSRGIXR5.js";export{b as HighlightLineNumbers,a as activateLineNumbers};
--- matchbox-server/src/main/resources/static/browser/chunk-HDFBKGUQ.js
+++
@@ -1,1 +0,0 @@
-import{Yc as a,Zc as b}from"./chunk-X7OI3EZF.js";import"./chunk-TSRGIXR5.js";export{b as HighlightLineNumbers,a as activateLineNumbers};
